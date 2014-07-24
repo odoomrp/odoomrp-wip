6fbc303d94ad94b7843f1f83d55effa1f8a329ba
avanzosc1 <avanzosc1@avanzosc1-HP-Compaq-6910p.(none)>
2014-07-24 18:20:00 +0200

    [ADD] sale order line menu

---
+++ avanzosc_newmenu_saleorderline/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Avanzosc - Avanced Open Source Consulting
+#    Copyright (C) 2011 - 2013 Avanzosc <http://www.avanzosc.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+{
+    "name": "Avanzosc New Menu For Sale Order Line",
+    "version": "1.0",
+    "depends": ["base", "sale"],
+    "author": "AvanzOSC",
+    "category": "Custom Modules",
+    "description": """
+           Avanzosc Custom Modules.
+           This module creates a new menu option for sales order lines.
+    """,
+    "init_xml": [],
+    'data': ['views/sale_order_line_ext_view.xml'],
+    'installable': True,
+}
