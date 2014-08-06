1048cb281bc42a363d9e45163c8729d7d5722208
avanzosc1 <avanzosc1@avanzosc1-HP-Compaq-6910p.(none)>
2014-08-06 11:19:16 +0200

    [ADD] warning_log

---
+++ warning_log/models/warning.py
@@ -0,0 +1,151 @@
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+from datetime import datetime
+
+
+class WarningLog(orm.Model):
+    _name = "warning.log"
+    _rec_name = 'user'
+    _columns = {
+        "date": fields.datetime("Date"),
+        "user": fields.many2one("res.users", "User"),
+        "msg": fields.text("Message"),
+        "type": fields.many2one("ir.model", "Model")}
+
+
+class SaleOrder(orm.Model):
+
+    _inherit = "sale.order"
+
+    def onchange_partner_id(self, cr, uid, ids, part, context=None):
+        vals = super(SaleOrder, self).onchange_partner_id(cr, uid, ids, part,
+                                                          context=context)
+        if 'warning' in vals and vals['warning']:
+            model_id = self.pool['ir.model'].search(
+                cr, uid, [('model', '=', self._name)], context=context)
+            self.pool['warning.log'].create(
+                cr, uid, {
+                    'date': datetime.now(), 'user': uid,
+                    'msg': 'warning' in vals and vals['warning']['message'],
+                    'type': model_id[0]}, context=context)
+        return vals
+
+
+class PurchaseOrder(orm.Model):
+    _inherit = 'purchase.order'
+
+    def onchange_partner_id(self, cr, uid, ids, part, context=None):
+        vals = super(PurchaseOrder, self).onchange_partner_id(
+            cr, uid, ids, part, context=context)
+        if 'warning' in vals and vals['warning']:
+            model_id = self.pool['ir.model'].search(
+                cr, uid, [('model', '=', self._name)], context=context)
+            self.pool['warning.log'].create(
+                cr, uid, {
+                    'date': datetime.now(), 'user': uid,
+                    'msg': 'warning' in vals and vals['warning']['message'],
+                    'type': model_id[0]}, context=context)
+        return vals
+
+
+class AccountInvoice(orm.Model):
+    _inherit = 'account.invoice'
+
+    def onchange_partner_id(self, cr, uid, ids, type, partner_id,
+                            date_invoice=False, payment_term=False,
+                            partner_bank_id=False, company_id=False,
+                            context=None):
+        vals = super(AccountInvoice, self).onchange_partner_id(
+            cr, uid, ids, part, context=context)
+        if 'warning' in vals and vals['warning']:
+            model_id = self.pool['ir.model'].search(
+                cr, uid, [('model', '=', self._name)], context=context)
+            self.pool['warning.log'].create(
+                cr, uid, {
+                    'date': datetime.now(), 'user': uid,
+                    'msg': 'warning' in vals and vals['warning']['message'],
+                    'type': model_id[0]}, context=context)
+        return vals
+
+
+class StockPicking(orm.Model):
+    _inherit = 'stock.picking'
+
+    def onchange_partner_in(self, cr, uid, ids, partner_id=None,
+                            context=None):
+        vals = super(StockPicking, self).onchange_partner_id(
+            cr, uid, ids, part, context=context)
+        if 'warning' in vals and vals['warning']:
+            model_id = self.pool['ir.model'].search(
+                cr, uid, [('model', '=', self._name)], context=context)
+            self.pool['warning.log'].create(
+                cr, uid, {
+                    'date': datetime.now(), 'user': uid,
+                    'msg': 'warning' in vals and vals['warning']['message'],
+                    'type': model_id[0]}, context=context)
+        return vals
+
+
+class saleOrderLine(orm.Model):
+    _inherit = 'sale.order.line'
+
+    def product_id_change_with_wh(self, cr, uid, ids, pricelist, product,
+                                  qty=0, uom=False, qty_uos=0, uos=False,
+                                  name='', partner_id=False, lang=False,
+                                  update_tax=True, date_order=False,
+                                  packaging=False, fiscal_position=False,
+                                  flag=False, warehouse_id=False,
+                                  context=None):
+        vals = super(saleOrderLine, self).onchange_partner_id(
+            cr, uid, ids, part, context=context)
+        if 'warning' in vals and vals['warning']:
+            model_id = self.pool['ir.model'].search(
+                cr, uid, [('model', '=', self._name)], context=context)
+            self.pool['warning.log'].create(
+                cr, uid, {
+                    'date': datetime.now(), 'user': uid,
+                    'msg': 'warning' in vals and vals['warning']['message'],
+                    'type': model_id[0]}, context=context)
+        return vals
+
+
+class PurchaseOrderLine(orm.Model):
+    _inherit = 'purchase.order.line'
+
+    def onchange_product_id(self, cr, uid, ids, pricelist, product, qty, uom,
+                            partner_id, date_order=False,
+                            fiscal_position_id=False, date_planned=False,
+                            name=False, price_unit=False, state='draft',
+                            notes=False, context=None):
+        vals = super(PurchaseOrderLine, self).onchange_partner_id(
+            cr, uid, ids, part, context=context)
+        if 'warning' in vals and vals['warning']:
+            model_id = self.pool['ir.model'].search(
+                cr, uid, [('model', '=', self._name)], context=context)
+            self.pool['warning.log'].create(
+                cr, uid, {
+                    'date': datetime.now(), 'user': uid,
+                    'msg': 'warning' in vals and vals['warning']['message'],
+                    'type': model_id[0]}, context=context)
+        return vals
