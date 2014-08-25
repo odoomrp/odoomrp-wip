840426eba7b530b561bf5aa92591f94a14b466d4
campos <danielcampos@avanzosc.com>
2014-08-25 18:33:31 +0200

    [ADD] <mrp_production_traceability>

---
+++ mrp_production_traceability /models/mrp_production.py
@@ -0,0 +1,43 @@
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Daniel Campos (danielcampos@avanzosc.es) Date: 25/08/2014
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+from openerp.osv import orm
+
+
+class MrpProduction(orm.Model):
+    _inherit = "mrp.production"
+
+    def action_produce(self, cr, uid, production_id, production_qty,
+                       production_mode, wiz=False, context=None):
+        st_move_obj = self.pool['stock.move']
+        move_ids_pre = st_move_obj.search(cr, uid,
+                                          [('raw_material_production_id', '=',
+                                            production_id)], context=context)
+        res = super(MrpProduction, self).action_produce(
+            cr, uid, production_id, production_qty, production_mode,
+            wiz=wiz, context=context)
+        if wiz.lot_id:
+            move_ids_post = st_move_obj.search(
+                cr, uid, [('raw_material_production_id', '=', production_id),
+                          ('id', 'not in', move_ids_pre)], context=context)
+            st_move_obj.write(cr, uid, move_ids_post,
+                              {'production_lot_id': wiz.lot_id.id})
+        return res
