f4bdf550bca494ea68e2797f57c2752b19b9916e
CsanchezC <csanchez@grupovermon.com>
2014-08-28 12:07:57 +0200

    Listo para corregir

---
+++ purchase_homologation/models/purchase_homologation.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#                                                                            #
+#  OpenERP, Open Source Management Solution.                                 #
+#                                                                            #
+#  @author Carlos Sánchez Cifuentes <csanchez@grupovermon.com>               #
+#                                                                            #
+#  This program is free software: you can redistribute it and/or modify      #
+#  it under the terms of the GNU Affero General Public License as            #
+#  published by the Free Software Foundation, either version 3 of the        #
+#  License, or (at your option) any later version.                           #
+#                                                                            #
+#  This program is distributed in the hope that it will be useful,           #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              #
+#  GNU Affero General Public License for more details.                       #
+#                                                                            #
+#  You should have received a copy of the GNU Affero General Public License  #
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.      #
+#                                                                            #
+##############################################################################
+
+from openerp.osv import fields, orm
+
+
+class PurchaseHomologation(orm.Model):
+
+    _name = 'purchase.homologation'
+
+    _description = 'Homologation for suppliers and categories'
+
+    _columns = {
+        'category_id': fields.many2one('product.category', 'Category'),
+        'comments': fields.text('Comments'),
+        'end_date': fields.datetime('Finishing date'),
+        'partner_id': fields.many2one('res.partner', 'Supplier', required=True),
+        'product_id': fields.many2one('product.product', 'Product', domain=[('purchase_ok', '=', True)]),
+        'start_date': fields.datetime('Beginning date'),
+    }
---
+++ purchase_homologation/models/purchase_order_line.py
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#                                                                            #
+#  OpenERP, Open Source Management Solution.                                 #
+#                                                                            #
+#  @author Carlos Sánchez Cifuentes <csanchez@grupovermon.com>               #
+#                                                                            #
+#  This program is free software: you can redistribute it and/or modify      #
+#  it under the terms of the GNU Affero General Public License as            #
+#  published by the Free Software Foundation, either version 3 of the        #
+#  License, or (at your option) any later version.                           #
+#                                                                            #
+#  This program is distributed in the hope that it will be useful,           #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              #
+#  GNU Affero General Public License for more details.                       #
+#                                                                            #
+#  You should have received a copy of the GNU Affero General Public License  #
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.      #
+#                                                                            #
+##############################################################################
+
+from openerp.osv import orm
+
+
+class PurchaseOrderLine(orm.Model):
+
+    _inherit = 'purchase.order.line'
+
+    def create(self, cr, uid, vals, context=None):
+        purchase_order = self.pool['purchase.order']
+        order_obj = purchase_order.browse(cr, uid, [vals['order_id']],
+                                          context=context)
+        product_product = self.pool['product.product']
+        product_obj = product_product.browse(cr, uid, [vals['product_id']],
+                                             context=context)
+        res_partner_approval = self.pool['purchase.homologation']
+        approval_ids = res_partner_approval.search(
+            cr, uid, [
+            ('partner_id', '=', order_obj[0].partner_id.id),
+            ('category_id', '=', product_obj[0].product_tmpl_id.categ_id.id),
+            ('start_date', '<', vals['date_planned']),
+            ('end_date', '>', vals['date_planned'])], context=context)
+        if approval_ids == False:
+            raise exceptions.Warning(
+                _('Error!'),
+                _("This product isn't homologate for the supplier selected."))
+        return super(PurchaseOrderLine, self).create(
+            cr, uid, vals, context=context)
