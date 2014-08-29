2f23983a8178267bb102d7b95fdefa0f10b932f8
avanzosc1 <avanzosc1@avanzosc1-HP-Compaq-6910p.(none)>
2014-08-29 13:48:28 +0200

    [ADD] project_events_operations

---
+++ project_events_operations/wizard/events_copy.py
@@ -0,0 +1,51 @@
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+from openerp import models, api, fields, exceptions
+from openerp.tools.translate import _
+from datetime import datetime
+
+
+class EventsCopy(models.TransientModel):
+    
+    _name = "events.copy"
+    _rec_name = "project_id"
+    project_id = fields.Many2one('project.project', string="Project")
+    start_date = fields.Datetime(string="Start Date")
+    
+    @api.multi
+    def copy_events(self):
+        event_obj = self.env['event.event']
+        events = event_obj.browse(self.env.context['active_ids']).copy()
+        for event in events:
+            if event.project_id.date_start:
+                #try:
+                diff_days =((self.start_date and datetime.strptime(self.start_date, "%Y-%m-%d %H:%M:%S")) or datetime.combine(datetime.strptime(self.project_id.date_start, "%Y-%m-%d"), datetime.min.time())) - datetime.combine(datetime.strptime(event.project_id.date_start, "%Y-%m-%d"), datetime.min.time())
+                event.write({
+                         'project_id': self.project_id.id,
+                         'date_begin': datetime.strptime(event.date_begin, "%Y-%m-%d %H:%M:%S") + diff_days,
+                         'date_end': datetime.strptime(event.date_end, "%Y-%m-%d %H:%M:%S") + diff_days
+                         })
+                #except Exception, e:
+                    #raise exceptions.Warning("Date not set", "it is not date on project or event")
+            else:
+                event.write({'project_id':self.project_id.id})
+        return{
+                'type': 'ir.actions.act_window_close',
+         }
---
+++ project_events_operations/wizard/events_date_modify.py
@@ -0,0 +1,50 @@
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+from openerp import api, models, fields
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+
+
+class EventsDateModify(models.TransientModel):
+    
+    _name = 'events.date.modify'
+    _rec_name = 'measure'
+    forward = fields.Boolean(string='Forward')
+    qty = fields.Integer(string='qty')
+    measure = fields.Selection([('days', 'Days'), ('months', 'Months')],
+                               string='Measure')
+    @api.multi
+    def modify_date(self):
+        event_obj = self.env['event.event']
+        events = event_obj.browse(self.env.context['active_ids'])
+        for event in events:
+            if self.measure=='months':
+                event.write({
+                         'date_begin': datetime.strptime(event.date_begin, "%Y-%m-%d %H:%M:%S") + relativedelta(months=(self.forward and -self.qty) or self.qty),
+                         'date_end': datetime.strptime(event.date_end, "%Y-%m-%d %H:%M:%S") + relativedelta(months=(self.forward and -self.qty) or self.qty),
+                         })
+            else:
+                event.write({
+                         'date_begin': datetime.strptime(event.date_begin, "%Y-%m-%d %H:%M:%S") + relativedelta(days=(self.forward and -self.qty) or self.qty),
+                         'date_end': datetime.strptime(event.date_end, "%Y-%m-%d %H:%M:%S") + relativedelta(days=(self.forward and -self.qty) or self.qty),
+                         })
+        return{
+                'type': 'ir.actions.act_window_close',
+         }
