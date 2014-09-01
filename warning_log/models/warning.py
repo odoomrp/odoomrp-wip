c3a6b6393fc9316dc6e73a7479131a4eceb9ddb7
avanzosc1 <avanzosc1@avanzosc1-HP-Compaq-6910p.(none)>
2014-09-01 10:52:23 +0200

    [FIX]

--- warning_log/models/warning.py
+++ warning_log/models/warning.py
@@ -119,7 +119,7 @@ class SaleOrderLine(orm.Model):
                                   packaging=False, fiscal_position=False,
                                   flag=False, warehouse_id=False,
                                   context=None):
-        vals = super(saleOrderLine, self).product_id_change_with_wh(
+        vals = super(SaleOrderLine, self).product_id_change_with_wh(
             cr, uid, ids, pricelist, product, qty=qty, uom=uom,
             qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
             lang=lang, update_tax=update_tax, date_order=date_order,
