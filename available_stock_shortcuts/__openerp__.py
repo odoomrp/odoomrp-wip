d195ce2f395a82e46682099c203f6d9986b7e5b5
avanzosc1 <avanzosc1@avanzosc1-HP-Compaq-6910p.(none)>
2014-09-01 10:49:54 +0200

    [ADD] available_stock_shortcuts

---
+++ available_stock_shortcuts/__openerp__.py
@@ -0,0 +1,34 @@
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+{
+    "name": "available_stock_shortcuts",
+    "version": "1.0",
+    "depends": ["base", "stock", "purchase", "mrp"],
+    "author": "OdooMRP team",
+    "contributors": ["Mikel Arregi <mikelarregi@avanzosc.es>"],
+    "category": "quants",
+    "description": """
+    Adds shorcut buttons to quants on manufacturing orders and puchase order.
+    """,
+    'data': ['views/purchase_view.xml',
+             'views/mrp_view.xml'
+             ],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ available_stock_shortcuts/models/purchase.py
@@ -0,0 +1,41 @@
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+from openerp import models, api
+
+
+class PurchaseOrder(models.Model):
+    
+    _inherit = 'purchase.order'
+    
+    @api.multi
+    def _get_products(self):
+        products = []
+        for purchase in self:
+            products += [x.product_id.id for x in purchase.order_line]
+        return products
+
+    @api.multi
+    def action_open_quants(self):
+        template_obj = self.env['product.template']
+        products = self._get_products()
+        result = template_obj._get_act_window_dict('stock.product_open_quants')
+        result['domain'] = "[('product_id','in',[" + ','.join(map(str, products)) + "])]"
+        result['context'] = "{'search_default_productgroup': 1, 'search_default_internal_loc': 1}"
+        return result
