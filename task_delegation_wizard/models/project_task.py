f220d139e1502002f038b462c17005fd6193e4b9
oihane <oihanecruce@gmail.com>
2014-09-09 11:05:38 +0200

    [MOD] añadida llamada a super

--- task_delegation_wizard/models/project_task.py
+++ task_delegation_wizard/models/project_task.py
@@ -30,31 +30,36 @@ class ProjectTask(orm.Model):
         assert (delegate_data['user_id'],
                 _("Delegated User should be specified"))
         delegated_tasks = {}
-
-        for task in self.browse(cr, uid, ids, context=context):
-            for i in range(delegate_data['split_in']):
-                delegated_task_id = self.copy(cr, uid, task.id, {
-                    'name': delegate_data['name'],
-                    'user_id': False,
-                    'project_id': (delegate_data['project_id'] and
-                                   delegate_data['project_id'][0] or False),
-                    'stage_id': task.stage_id.id,
-                    'planned_hours': delegate_data['planned_hours'] or 0.0,
-                    'remaining_hours': delegate_data['planned_hours'] or 0.0,
-                    'parent_ids': [(6, 0, [task.id])],
-                    'description': delegate_data['new_task_description'] or '',
-                    'child_ids': [],
-                    'work_ids': [],
+        if not 'split_in' in delegate_data or not delegate_data['split_in']:
+            delegated_tasks = super(ProjectTask, self).do_delegate(
+                cr, uid, ids, delegate_data=delegate_data, contex=context)
+        else:
+            for task in self.browse(cr, uid, ids, context=context):
+                for i in range(delegate_data['split_in']):
+                    delegated_task_id = self.copy(cr, uid, task.id, {
+                        'name': delegate_data['name'],
+                        'user_id': False,
+                        'project_id': (delegate_data['project_id'] and
+                                       delegate_data['project_id'][0] or False),
+                        'stage_id': task.stage_id.id,
+                        'planned_hours': delegate_data['planned_hours'] or 0.0,
+                        'remaining_hours': (delegate_data['planned_hours'] or 
+                                            0.0),
+                        'parent_ids': [(6, 0, [task.id])],
+                        'description': (delegate_data['new_task_description']
+                                        or ''),
+                        'child_ids': [],
+                        'work_ids': [],
+                    }, context=context)
+                    self._delegate_task_attachments(cr, uid, task.id,
+                                                    delegated_task_id,
+                                                    context=context)
+    
+                remain = (delegate_data['split_in'] *
+                          delegate_data['planned_hours_me'])
+                task.write({
+                    'name': delegate_data['prefix'],
+                    'remaining_hours': remain,
                 }, context=context)
-                self._delegate_task_attachments(cr, uid, task.id,
-                                                delegated_task_id,
-                                                context=context)
-
-            remain = (delegate_data['split_in'] *
-                      delegate_data['planned_hours_me'])
-            task.write({
-                'name': delegate_data['prefix'],
-                'remaining_hours': remain,
-            }, context=context)
-            delegated_tasks[task.id] = delegated_task_id
+                delegated_tasks[task.id] = delegated_task_id
         return delegated_tasks
