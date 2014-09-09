2c1305d25dff7a8a4152ee9b815daa2649680521
avanzosc1 <avanzosc1@avanzosc1-HP-Compaq-6910p.(none)>
2014-09-09 17:14:14 +0200

    [IMP] operations time control from work order

--- operation_time_control/models/operation_time.py
+++ operation_time_control/models/operation_time.py
@@ -26,17 +26,51 @@ from datetime import datetime
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 
-class OperationTime(models.Model):
+class MrpProductionWorkcenterLine(models.Model):
 
     _inherit = 'mrp.production.workcenter.line'
-    operation_time_lines = fields.One2many('operation.time.lines',
+    operation_time_lines = fields.One2many('operation.time.line',
                                            'operation_time')
 
+    def _create_operation_line(self):
+        self.env['operation.time.line'].create({
+            'start_date': fields.Datetime.now(),
+            'operation_time': self.id,
+            'user': self.env.uid})
 
-class OperationTimeLines(models.Model):
+    def _write_end_date_operation_line(self):
+        self.operation_time_lines[-1].end_date = fields.Datetime.now()
 
-    _name = 'operation.time.lines'
+    def action_start_working(self):
+        result = super(MrpProductionWorkcenterLine,
+                       self).action_start_working()
+        self._create_operation_line()
+        return result
+
+    def action_pause(self):
+        result = super(MrpProductionWorkcenterLine, self).action_pause()
+        self._write_end_date_operation_line()
+        return result
+
+    def action_resume(self):
+        result = super(MrpProductionWorkcenterLine, self).action_resume()
+        self._create_operation_line()
+        return result
+
+    def action_done(self):
+        result = super(MrpProductionWorkcenterLine, self).action_done()
+        self._write_end_date_operation_line()
+        return result
+
+
+class OperationTimeLine(models.Model):
+
+    _name = 'operation.time.line'
     _rec_name = 'operation_time'
+
+    def _default_user(self):
+        return self.env.uid
+
     start_date = fields.Datetime(string='Start Date')
     end_date = fields.Datetime(string='End Date')
     operation_time = fields.Many2one('mrp.production.workcenter.line')
@@ -45,16 +79,15 @@ class OperationTimeLines(models.Model):
     production = fields.Many2one('mrp.production',
                                  related='operation_time.production_id',
                                  string='Production', store=True)
+    user = fields.Many2one('res.users', string='User', default=_default_user)
 
     @api.one
     @api.depends('start_date', 'end_date')
     def operation_uptime(self):
         if self.end_date and self.start_date:
-            timedelta = datetime.strptime(self.end_date,
-                                          DEFAULT_SERVER_DATETIME_FORMAT) - \
-                datetime.strptime(self.start_date,
-                                  DEFAULT_SERVER_DATETIME_FORMAT)
-            self.uptime = timedelta.total_seconds()/3600.
+            timedelta = fields.Datetime.from_string(self.end_date) - \
+                fields.Datetime.from_string(self.start_date)
+            self.uptime = timedelta.total_seconds() / 3600.
         else:
             self.uptime = 0
         return True
