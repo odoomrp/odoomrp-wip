b530306d4fd956fc794c89abe821e0fc67469759
oihane <oihanecruce@gmail.com>
2014-09-15 09:28:59 +0200

    [MOD] __openerp__.py

--- purchase_requisition_ext/__openerp__.py
+++ purchase_requisition_ext/__openerp__.py
@@ -1,9 +1,6 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Avanzosc - Avanced Open Source Consulting
-#    Copyright (C) 2011 - 2014 Avanzosc <http://www.avanzosc.com>
-#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
@@ -18,20 +15,28 @@
 #    along with this program.  If not, see http://www.gnu.org/licenses/.
 #
 ##############################################################################
+
 {
-    "name": "Purchase Requisition Ext",
+    "name": "Purchase Requisition Extension",
     "version": "1.0",
-    "author": "AvanzOSC",
-    "category": "Custom Modules",
-    "website": "www.avanzosc.es",
+    "depends": [
+        "purchase_requisition",
+    ],
+    "author": "OdooMRP team",
+    "contributors": [
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+    ],
+    "category": "Purchase Management",
+    "website": "http://www.odoomrp.com",
+    "summary": "",
     "description": """
-    Este módulo relaciona la línea de pedido de compra, con la solicitud de
-    compra.
-    Además en la solicitud de compra crea una nueva pestaña, en la que muestra
-    todas las líneas de compra generadas.
+Este módulo relaciona la línea de pedido de compra, con la solicitud de
+compra.
+Además en la solicitud de compra crea una nueva pestaña, en la que muestra
+todas las líneas de compra generadas.
     """,
-    "depends": ['purchase_requisition'],
-    'data': ['views/purchase_requisition_ext_view.xml',
-             ],
-    'installable': True,
+    "data": [
+        "views/purchase_requisition_ext_view.xml",
+    ],
+    "installable": True,
 }
