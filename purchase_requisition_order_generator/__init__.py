0fa832566b99f0c5da827423d898f556595845f6
oihane <oihanecruce@gmail.com>
2014-09-16 12:22:05 +0200

    Update __init__.py

--- purchase_requisition_order_generator/__init__.py
+++ purchase_requisition_order_generator/__init__.py
@@ -1,9 +1,6 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Avanzosc - Avanced Open Source Consulting
-#    Copyright (C) 2011 - 2014 Avanzosc <http://www.avanzosc.com>
-#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
@@ -18,4 +15,5 @@
 #    along with this program.  If not, see http://www.gnu.org/licenses/.
 #
 ##############################################################################
+
 from . import models
