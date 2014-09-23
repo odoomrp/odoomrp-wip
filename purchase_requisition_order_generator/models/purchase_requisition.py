a4ff9c61380ba1e4fd0f0370a8d5ab5b5e25139d
oihane <oihanecruce@gmail.com>
2014-09-23 14:51:05 +0200

    [MOD] Evitar creacion de PO vacios

--- purchase_requisition_order_generator/models/purchase_requisition.py
+++ purchase_requisition_order_generator/models/purchase_requisition.py
@@ -16,8 +16,12 @@
 #
 ##############################################################################
 
+import logging
+
 from openerp import models, api, exceptions
-from openerp.tools.translate import _
+from openerp import _
+
+_logger = logging.getLogger(__name__)
 
 
 class PurchaseRequisition(models.Model):
@@ -25,14 +29,11 @@ class PurchaseRequisition(models.Model):
 
     @api.multi
     @api.model
-    def make_purchase_order_avanzosc(self):
+    def generate_purchase_orders(self):
         purchase_obj = self.env['purchase.order']
         purchase_line_obj = self.env['purchase.order.line']
-        partner_obj = self.env['res.partner']
         supplierinfo_obj = self.env['product.supplierinfo']
-        res = {}
         for requisition in self:
-            purchase_order_datas = []
             for line in requisition.line_ids:
                 product = line.product_id
                 condition = [('product_tmpl_id', '=',
@@ -45,18 +46,23 @@ class PurchaseRequisition(models.Model):
                 else:
                     for supplierinfo in supplierinfos:
                         supplier = supplierinfo.name
-                        condition = [('partner_id', '=', supplier.id),
-                                     ('state', '=', 'draft'),
-                                     ('requisition_id', '=', requisition.id)]
-                        purchase_order = purchase_obj.search(condition)
-                        if not purchase_order:
-                            vals = self._prepare_purchase_order(requisition,
-                                                                supplier)
-                            purchase = purchase_obj.create(vals)
-                            purchase_order_datas.append(purchase)
-                        else:
-                            purchase = purchase_order[0]
-                        vals = self._prepare_purchase_order_line(requisition, line, purchase.id, supplier)
-                        purchase_line_obj.create(vals)
-            res[requisition.id] = purchase_order_datas
+                        try:
+                            vals = self._prepare_purchase_order_line(
+                                requisition, line, False, supplier)
+                            condition = [('partner_id', '=', supplier.id),
+                                         ('state', '=', 'draft'),
+                                         ('requisition_id', '=',
+                                          requisition.id)]
+                            purchase_order = purchase_obj.search(condition)
+                            if not purchase_order:
+                                po_vals = self._prepare_purchase_order(
+                                    requisition, supplier)
+                                purchase = purchase_obj.create(po_vals)
+                            else:
+                                purchase = purchase_order[0]
+                            vals.update({'order_id': purchase.id})
+                            purchase_line_obj.create(vals)
+                        except:
+                            _logger.info(_('Supplier: %s - Product: %s') %
+                                         (supplier.name, product.name))
         return True
