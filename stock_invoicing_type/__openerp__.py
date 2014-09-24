7e94787c139d61516ecd950d9b7ffe818ea4a8e5
Ainara <ainara@ainara-Presario-CQ57.(none)>
2014-09-24 17:27:01 +0200

    [ADD] stock_invoicing_type

---
+++ stock_invoicing_type/__openerp__.py
@@ -0,0 +1,35 @@
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+{
+    "name": "Invoicing type on pickings",
+    "version": "1.0",
+    "depends": ["sale_journal", "stock_account"],
+    "author": "OdooMRP team",
+    "contributors": ["Mikel Arregi <mikelarregi@avanzosc.es>",
+                     "Ainara Galdona <ainaragaldona@avanzosc.es>"],
+    "category": "stock_picking",
+    "description": """
+        Carry invoicing type from sale order to stock picking.
+        Wizard Create invoice from picking: 
+            Group by partner according to invoicing type's config.
+    """,
+    'data': ["wizard/stock_invoice_onshipping_view.xml"],
+    "installable": True,
+    "auto_install": False,
+}
