9121350fa8ddbb13972538f7078f7c515cbead9b
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-10-09 19:16:20 +0200

    [IMP] quality_control: Mejora del código

--- quality_control_between_dates/models/qc_test.py
+++ quality_control_between_dates/models/qc_test.py
@@ -16,86 +16,38 @@
 #
 ##############################################################################
 from openerp import models, fields, api, exceptions, _
-from datetime import date
 
 
 class QcTestTemplateLine(models.Model):
     _inherit = 'qc.test.template.line'
 
-    validity_start_date = fields.Date(string='Validity Start Date',
-                                      required=True)
-    validity_end_date = fields.Date(string='Validity End Date')
+    date_start = fields.Date(string='Validity Start Date')
+    date_end = fields.Date(string='Validity End Date')
 
     @api.one
-    @api.constrains('validity_start_date', 'validity_end_date',
-                    'test_template_id')
-    def _check_validity_end_date(self):
-        start_date = self.validity_start_date
-        end_date = self.validity_end_date
-        if not self.validity_end_date and self.test_template_id:
-            if self.test_template_id.test_template_line_ids:
-                for line in self.test_template_id.test_template_line_ids:
-                    if (line.id != self.id and not line.validity_end_date and
-                            line.proof_id.id == self.proof_id.id):
-                        raise exceptions.Warning(_('You must include Validity'
-                                                   'End Date in the previous'
-                                                   ' lines of that question.'))
-                    if line.validity_end_date:
-                        if (line.id != self.id and
-                            line.proof_id.id == self.proof_id.id and
-                            start_date >= line.validity_start_date and
-                                start_date <= line.validity_end_date):
-                            raise exceptions.Warning(_('You must review date'
-                                                       ' ranges, validity'
-                                                       ' start date Error.'))
-                    else:
-                        if (line.id != self.id and
-                            line.proof_id.id == self.proof_id.id and
-                                start_date >= line.validity_start_date):
-                            raise exceptions.Warning(_('You must review date'
-                                                       ' ranges, validity'
-                                                       ' start date Error.'))
-        else:
-            if start_date and self.test_template_id:
-                if self.test_template_id.test_template_line_ids:
-                    for line in self.test_template_id.test_template_line_ids:
-                        if line.validity_end_date:
-                            if (line.id != self.id and
-                                line.proof_id.id == self.proof_id.id and
-                                start_date >= line.validity_start_date and
-                                    start_date <= line.validity_end_date):
-                                raise exceptions.Warning(_('You must review'
-                                                           ' date ranges,'
-                                                           ' validity start'
-                                                           ' date Error.'))
-                        else:
-                            if (line.id != self.id and
-                                line.proof_id.id == self.proof_id.id and
-                                    start_date >= line.validity_start_date):
-                                raise exceptions.Warning(_('You must review'
-                                                           ' date ranges,'
-                                                           ' validity start'
-                                                           ' date Error.'))
-            if end_date and self.test_template_id:
-                if self.test_template_id.test_template_line_ids:
-                    for line in self.test_template_id.test_template_line_ids:
-                        if line.validity_end_date:
-                            if (line.id != self.id and
-                                line.proof_id.id == self.proof_id.id and
-                                end_date >= line.validity_start_date and
-                                    end_date <= line.validity_end_date):
-                                raise exceptions.Warning(_('You must review'
-                                                           ' date ranges,'
-                                                           ' validity start'
-                                                           ' date Error.'))
-                        else:
-                            if (line.id != self.id and
-                                line.proof_id.id == self.proof_id.id and
-                                    end_date >= line.validity_start_date):
-                                raise exceptions.Warning(_('You must review'
-                                                           ' date ranges,'
-                                                           ' validity start'
-                                                           ' date Error.'))
+    @api.constrains('date_start', 'date_end')
+    def _check_interval(self):
+        if (self.date_start and self.date_end and
+                self.date_start > self.date_end):
+            raise exceptions.Warning(_('Validity start date cannot be greater '
+                                       'than end date.'))
+
+    @api.one
+    @api.constrains('date_start', 'date_end', 'test_template_id')
+    def _check_line_validities(self):
+        domain = [('id', '!=', self.id), ('proof_id', '=', self.proof_id)]
+        if self.date_start:
+            domain.append('|')
+            domain.append(('date_end', '>=', self.date_start))
+            domain.append(('date_end', '=', False))
+        if self.date_end:
+            domain.append('|')
+            domain.append(('date_start', '<=', self.date_end))
+            domain.append(('date_start', '=', False))
+        other_lines = self.search(domain)
+        if other_lines:
+            raise exceptions.Warning(_('You cannot have 2 lines of the same '
+                                       'question that overlap!'))
 
 
 class QcTest(models.Model):
@@ -103,29 +55,18 @@ class QcTest(models.Model):
 
     def _prepare_test_lines(self, cr, uid, test, force_fill=False,
                             context=None):
-        templine_obj = self.pool['qc.test.template.line']
-        new_data = super(QcTest, self)._prepare_test_lines(
+        new_data = []
+        tmpl_line_obj = self.pool['qc.test.template.line']
+        now = fields.Date.context_today(self)
+        data = super(QcTest, self)._prepare_test_lines(
             cr, uid, test, force_fill=force_fill, context=context)
-        if new_data:
-            for line in new_data:
-                test_line = line[2]
-                templine_id = test_line.get('test_template_line_id', False)
-                if templine_id:
-                    templine = templine_obj.browse(cr, uid, templine_id,
-                                                   context)
-                    start_date = fields.Date.from_string(
-                        templine.validity_start_date)
-                    if not templine.validity_end_date:
-                        end_date = False
-                    else:
-                        end_date = fields.Date.from_string(
-                            templine.validity_end_date)
-                    if date.today() < start_date:
-                        id = new_data.index(line)
-                        new_data.pop(id)
-                    else:
-                        if end_date:
-                            if date.today() > end_date:
-                                id = new_data.index(line)
-                                new_data.pop(id)
+        for line_dict in data:
+            tmpl_line_id = line_dict[2].get('test_template_line_id', False)
+            if tmpl_line_id:
+                tmpl_line = tmpl_line_obj.browse(cr, uid, tmpl_line_id,
+                                                 context=context)
+                if ((not tmpl_line.date_start or
+                        tmpl_line.date_start <= now) and
+                        (not tmpl_line.date_end or tmpl_line.date_end >= now)):
+                    new_data.append(line_dict)
         return new_data
