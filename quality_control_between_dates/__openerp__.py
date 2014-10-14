916cd35782d68eb880bc2cdba8efdd3e20d46749
oihane <oihanecruce@gmail.com>
2014-10-14 10:43:34 +0200

    [MOD] __openerp__.py file fixed

--- quality_control_between_dates/__openerp__.py
+++ quality_control_between_dates/__openerp__.py
@@ -15,6 +15,7 @@
 #    along with this program.  If not, see http://www.gnu.org/licenses/.
 #
 ##############################################################################
+
 {
     "name": "Quality Control Between Dates",
     "version": "1.0",
