868723166f117188e40b4bc2f8f0c1176f79849d
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2014-10-17 12:26:03 +0200

    [FIX mrp_bom_by_percentage]

---
+++ mrp_bom_by_percentage/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+{
+    "name": "MRP Bom By Percentage",
+    "version": "1.0",
+    "author": "OdooMRP team",
+    "category": "MRP",
+    "website": "http://www.odoomrp.com",
+    "description": """
+    This module performs the following:
+
+    Include in the header of the Bom list, two new fields:
+
+        1.- Produce by percentage: Type boolean, when you check this new field,
+            the quantity to be produced is 100, and the field 'amount to
+            produce' will be invisible.
+
+        2.- QTY to consume. Calculated field. Displays the sum of all amounts
+            to consume.
+
+    If the BoM list is by_percentage, will be validated that the new field
+    'QTY to consume' is not greater than 100.
+    """,
+    "depends": ['mrp',
+                ],
+    "data": ['views/mrp_bom_view.xml',
+             ],
+    "installable": True
+}
---
+++ mrp_bom_by_percentage/models/mrp_bom.py
@@ -0,0 +1,48 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from openerp import models, fields, api, exceptions, _
+import openerp.addons.decimal_precision as dp
+
+
+class MrpBom(models.Model):
+    _inherit = 'mrp.bom'
+
+    @api.one
+    @api.depends('bom_line_ids')
+    def _compute_qtytoconsume(self):
+        qty_to_consume = 0
+        if self.bom_line_ids:
+            for line in self.bom_line_ids:
+                qty_to_consume += line.product_qty
+        self.qty_to_consume = qty_to_consume
+
+    by_percentage = fields.Boolean(string='Produce by percentage')
+    qty_to_consume = fields.Float(
+        string='QTY to consume', compute='_compute_qtytoconsume', store=True,
+        digits=dp.get_precision('Product Unit of Measure'))
+
+    @api.onchange('by_percentage')
+    def onchange_by_percentage(self):
+        if self.by_percentage:
+            self.product_qty = 100
+
+    @api.one
+    @api.constrains('by_percentage', 'qty_to_consume', 'bom_line_ids')
+    def _check_by_percentage(self):
+        if self.by_percentage and self.qty_to_consume != 100:
+            raise exceptions.Warning(_('Quantity to consume <> 100'))
