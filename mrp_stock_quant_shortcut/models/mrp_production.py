ff397940aa29f87d0221d80c088c810fa18b4200
oihane <oihanecruce@gmail.com>
2014-10-28 16:50:34 +0100

    [MOD] dictionary in context

--- mrp_stock_quant_shortcut/models/mrp_production.py
+++ mrp_stock_quant_shortcut/models/mrp_production.py
@@ -34,6 +34,6 @@ class MrpProduction(models.Model):
             products |= line.product_id
         result = template_obj._get_act_window_dict('stock.product_open_quants')
         result['domain'] = "[('product_id', 'in', " + str(products.ids) + ")]"
-        result['context'] = ({'search_default_productgroup': 1,
-                              'search_default_internal_loc': 1})
+        result['context'] = {'search_default_productgroup': 1,
+                             'search_default_internal_loc': 1}
         return result
--- purchase_stock_quant_shortcut/models/purchase_order.py
+++ purchase_stock_quant_shortcut/models/purchase_order.py
@@ -30,6 +30,6 @@ class PurchaseOrder(models.Model):
             products |= line.product_id
         result = template_obj._get_act_window_dict('stock.product_open_quants')
         result['domain'] = "[('product_id', 'in', " + str(products.ids) + ")]"
-        result['context'] = ({'search_default_productgroup': 1,
-                              'search_default_internal_loc': 1})
+        result['context'] = {'search_default_productgroup': 1,
+                             'search_default_internal_loc': 1}
         return result
