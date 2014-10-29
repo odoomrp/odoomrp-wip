11c9b693b0c63cd6c550f0cb317427977cafa79b
campos <danielcampos@avanzosc.com>
2014-10-29 10:34:53 +0100

    [ADD] partner_control_fields_view

---
+++ partner_control_fields_view/__openerp__.py
@@ -0,0 +1,15 @@
+
+{
+    'name': 'Partner Control Fields View',
+    'version': '1.0',
+    'description': """
+    This module shows the invisible fields "created by" and "last updated by"
+    in partner views.
+    """,
+    'author': 'OdooMRP team',
+    'website': 'http://www.avanzosc.com',
+    "depends": ['base'],
+    "category": "Generic Modules",
+    "data": ['views/res_partner_view.xml'],
+    "installable": True,
+}
