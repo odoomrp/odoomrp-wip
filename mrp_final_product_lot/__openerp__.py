71ccc5e36f7d26d0f8d49d2fa66a51fddf4125ea
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2014-10-29 17:00:48 +0100

    [IMP mrp_final_product_lot]

--- mrp_final_product_lot/__openerp__.py
+++ mrp_final_product_lot/__openerp__.py
@@ -25,9 +25,16 @@
     This module automatically creates the lot to the final product of the
     production order.
 
-    In the MRP production object, one new field is added: Manual Production
-    Lot. If this field has a value, this value was used to create the lot, but
-    also the concatenation of all lots consumed is used.
+    In the MRP production object, two new fields are added: Manual Production
+    Lot of type char, and Concatenate Lots Components of type boolean.
+
+    The lot code is generated:
+
+    1.- If the new field Manual Production lot has value: This data, if not the
+        order number.
+    2.- If the check Concatenate Lots Components, this clicked, to the previous
+        point code is concatenated all lot numbers of all components used to
+        make the final product.
     """,
     "depends": ['stock',
                 'mrp',
--- mrp_final_product_lot/models/mrp_production.py
+++ mrp_final_product_lot/models/mrp_production.py
@@ -22,6 +22,8 @@ class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     manual_production_lot = fields.Char(string='Manual Production Lot')
+    concatenate_lots_components = fields.Boolean(
+        string='Concatenate Lots Components')
 
     def action_produce(self, cr, uid, production_id, production_qty,
                        production_mode, wiz=False, context=None):
@@ -32,55 +34,35 @@ class MrpProduction(models.Model):
             if (production.product_id.track_all or
                 production.product_id.track_production or
                     production.product_id.track_incoming):
-                lot = False
+                lot_id = False
                 for line in production.move_created_ids2:
                     if (line.product_id.id == production.product_id.id and
                             line.restrict_lot_id):
-                        lot = line.restric_lot_id
-                if not lot:
-                    if production.manual_production_lot:
-                        code = production.manual_production_lot
-                    else:
+                        lot_id = line.restric_lot_id.id
+                if not lot_id:
+                    code = (production.manual_production_lot, production.name)
+                    if production.concatenate_lots_components:
                         lot_datas = {}
                         for line in wiz.consume_lines:
                             if line.lot_id:
-                                found = False
-                                for data in lot_datas:
-                                    datos_array = lot_datas[data]
-                                    lot_id = datos_array['lot_id']
-                                    if lot_id == line.lot_id.id:
-                                        found = True
-                                if not found:
-                                    my_vals = {'lot_id': line.lot_id.id,
-                                               }
-                                    lot_datas[(line.lot_id.id)] = (
-                                        my_vals)
+                                lot_datas = self._find_component_lot(
+                                    cr, uid, lot_datas, line.lot_id,
+                                    context=context)
                         for line in production.move_lines2:
                             if line.restrict_lot_id:
-                                found = False
-                                for data in lot_datas:
-                                    datos_array = lot_datas[data]
-                                    lot_id = datos_array['lot_id']
-                                    if lot_id == line.restrict_lot_id.id:
-                                        found = True
-                                if not found:
-                                    my_vals = {'lot_id':
-                                               line.restrict_lot_id.id,
-                                               }
-                                    lot_datas[(line.restrict_lot_id.id)] = (
-                                        my_vals)
-                        code = False
+                                lot_datas = self._find_component_lot(
+                                    cr, uid, lot_datas, line.restrict_lot_id,
+                                    context=context)
+                        lots_components = ''
                         for data in lot_datas:
                             datos_array = lot_datas[data]
                             lot_id = datos_array['lot_id']
                             lot = lot_obj.browse(cr, uid, lot_id,
                                                  context=context)
-                            if not code:
-                                code = lot.name
-                            else:
-                                code += '-' + lot.name
-                        vals = {'name': code,
-                                'product_id': production.product_id.id}
+                            lots_components += '-' + lot.name
+                        code += lots_components
+                    vals = {'name': code,
+                            'product_id': production.product_id.id}
                     lot_id = lot_obj.create(cr, uid, vals, context=context)
                 vals = {'lot_id': lot_id}
                 product_produce_obj.write(cr, uid, wiz.id, vals,
@@ -88,3 +70,15 @@ class MrpProduction(models.Model):
         return super(MrpProduction, self).action_produce(
             cr, uid, production_id, production_qty, production_mode, wiz=wiz,
             context=context)
+
+    def find_component_lot(self, cr, uid, lot_datas, lot, context=None):
+        found = False
+        for data in lot_datas:
+            datos_array = lot_datas[data]
+            lot_id = datos_array['lot_id']
+            if lot_id == lot.id:
+                found = True
+        if not found:
+            my_vals = {'lot_id': lot.id}
+            lot_datas[(lot.id)] = (my_vals)
+        return lot_datas
