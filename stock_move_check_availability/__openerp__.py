18a3799bd769fe52e8957ccddf0ad47aecb47beb
campos <danielcampos@avanzosc.com>
2014-10-29 12:59:43 +0100

    [FIX] website

--- stock_move_check_availability/__openerp__.py
+++ stock_move_check_availability/__openerp__.py
@@ -6,7 +6,7 @@
     This module adds a button to check availability in stock moves.
     """,
     'author': 'OdooMRP team',
-    'website': 'http://www.avanzosc.com',
+    'website': 'http://www.avanzosc.es',
     "depends": ['stock'],
     "category": "Generic Modules",
     "data": ['views/stock_move_view.xml'
