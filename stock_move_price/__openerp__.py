b6c97da4c89b11862880248c1e36fe8086a4b2d3
avanzosc1 <avanzosc1@avanzosc1-HP-Compaq-6910p.(none)>
2014-10-29 16:22:17 +0100

    [mod]

--- stock_move_price/__openerp__.py
+++ stock_move_price/__openerp__.py
@@ -3,27 +3,27 @@
 ##############################################################################
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see http://www.gnu.org/licenses/.
 #
 ##############################################################################
 
 {
-    "name": "stock_picking_line_prices_info",
+    "name": "Stock Move Price",
     "version": "1.0",
     "depends": ["base", "stock", "purchase"],
     "author": "OdooMRP team",
     "contributors": ["Mikel Arregi <mikelarregi@avanzosc.es>"],
-    "category": "stock",
+    "category": "purchase",
     "description": """
     Adds price column on backorder lines
     """,
