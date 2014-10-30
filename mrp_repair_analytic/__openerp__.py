ad925809055aff6866020b0bc59ffde2d2483395
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2014-10-30 09:17:42 +0100

    [IMP mrp_repair_analytic]

--- mrp_repair_analytic/__openerp__.py
+++ mrp_repair_analytic/__openerp__.py
@@ -24,9 +24,9 @@
     "description": """
     This module performs the following:
 
-    1.- Is created the new field 'Analytic Account' in the object MRP Repair.
+    1.- It creates a new field 'Analytic Account' in the object MRP Repair.
 
-    2.- Is created the new filed 'User' in the operations, and components of
+    2.- It creates a new filed 'User' in the operations, and components of
         the repair order.
 
     When the repair order is confirmed, for each line of operations, and
