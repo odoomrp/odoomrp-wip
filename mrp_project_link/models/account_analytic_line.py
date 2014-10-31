dd1dee5f9a6872385740017de80efe6e4c746abc
Ainara <ainara@ainara-Presario-CQ57.(none)>
2014-10-31 10:00:47 +0100

    [IMP] añadir campo task_id en líneas analíticas

--- mrp_project_link/models/account_analytic_line.py
+++ mrp_project_link/models/account_analytic_line.py
@@ -26,3 +26,4 @@ class AccountAnalyticLine(models.Model):
     mrp_production_id = fields.Many2one('mrp.production',
                                         string='Manufacturing Order')
     workorder = fields.Many2one('mrp.production.workcenter.line', 'Work Order')
+    task_id = fields.Many2one('project.task', 'Project Task')
