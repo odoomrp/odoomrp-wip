a804984028872bbfb3717d86607fe266a90d5778
oihane <oihanecruce@gmail.com>
2014-11-05 17:43:01 +0100

    [NEW] Product attribute line views

---
+++ product_attribute_line_view/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+{
+    "name": "Product attribute lines views",
+    "version": "1.0",
+    "depends": [
+        "product",
+    ],
+    "author": "OdooMRP team",
+    "contributors": [
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+    ],
+    "category": "Custom Module",
+    "website": "http://www.odoomrp.com",
+    "summary": "",
+    "description": """
+This module provide :
+
+* A menu item to access attribute lines
+* Views for attribute lines
+
+    """,
+    "data": [
+        "views/product_attribute_line_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
