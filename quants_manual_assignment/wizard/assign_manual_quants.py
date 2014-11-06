525c16a869abaf081ba9f12f5c73362e871a001b
avanzosc1 <avanzosc1@avanzosc1-HP-Compaq-6910p.(none)>
2014-11-06 17:13:20 +0100

    [mod] pep8

--- quants_manual_assignment/wizard/assign_manual_quants.py
+++ quants_manual_assignment/wizard/assign_manual_quants.py
@@ -26,13 +26,11 @@ class AssignManualQuants(models.TransientModel):
 
     quants_lines = fields.One2many('assign.manual.quants.lines', 'move',
                                    string='Quants')
-#    unassign_lines = fields.One2many('unassign.manual.quants.lines', 'move',
-#                                     string='Confirm Quants')
 
     @api.multi
     def assign_quants(self):
         move = self.env['stock.move'].browse(self.env.context['active_ids'][0])
-        quants=[]
+        quants = []
         for line in self.quants_lines:
             if line.selected:
                 quants.append(line.quant.id)
@@ -41,7 +39,7 @@ class AssignManualQuants(models.TransientModel):
         return {}
 
     def default_get(self, cr, uid, fiels, context=None):
-        unassign_lines=[]
+        unassign_lines = []
         move = self.pool['stock.move'].browse(
             cr, uid, context['active_ids'][0], context=context)
         available_quants_ids = self.pool['stock.quant'].search(
@@ -51,49 +49,18 @@ class AssignManualQuants(models.TransientModel):
                 ('product_id', '=', move.product_id.id),
                 ('qty', '>', 0),
                 ('reservation_id', '=', False)], context=context)
-#        if move.reserved_quant_ids:
-#            unassign_lines = [{'quant': x.id} for x in move.reserved_quant_ids]
         available_quants = [{'quant': x} for x in available_quants_ids]
         available_quants.extend(
             {'quant': x,
              'selected': True
              } for x in move.reserved_quant_ids.ids)
         return {'quants_lines': available_quants}
-    
-#    @api.one
-#    def assign_quant(self):
-#        quants=[]
-#        for line in self.quants_lines:
-#            if line.selected:
-#                quants.append(line.quant.id)
-#                line.unlink()
-#        self.unassign_lines= map(lambda x: (0,0,{'quant': x}),quants)
-#        return {'nodestroy': True}
 
 
 class AssignManualQuantsLines(models.TransientModel):
     _name = 'assign.manual.quants.lines'
     _rec_name = 'quant'
 
-#    @api.onchange('selected')
-#    def assign_quant(self):
-#        self.move.unassign_lines= [(0,0,{'quant': self.quant.id})]
-#        #self.unlink()
-
     move = fields.Many2one('assign.manual.quants', string='Move')
     quant = fields.Many2one('stock.quant', string="Quant")
     selected = fields.Boolean(string="Select")
-
-
-#class UnassignManualQuantsLines(models.TransientModel):
-#    _name = 'unassign.manual.quants.lines'
-#    _rec_name = 'quant'
-#    move = fields.Many2one('assign.manual.quants', string='Move')
-#    quant = fields.Many2one('stock.quant', string="Quant")
-#
-#    @api.one
-#    def unassign_quant(self):
-#        self.move.write({'quants_lines': [(0, 0,
-#                                           {'move': self.move.id,
-#                                            'quant': self.quant.id})]})
-#        self.unlink()
