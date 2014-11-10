d01f5e9010d8424fd350c75033ee1df359ac61a1
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2014-11-10 10:18:49 +0100

    [FIX mrp_bom_version]

--- mrp_bom_version/__openerp__.py
+++ mrp_bom_version/__openerp__.py
@@ -24,12 +24,10 @@
     "description": """
     This module performs the following:
 
-    1.- In the MRP BoM list object, 3 new fields are added:
+    1.- In the MRP BoM list object, 2 new fields are added:
 
-        1.1.- Review, of type integer, defined by hand. When saving the list
-              verified that there is another with the same sequence.
-        1.2.- Historical Date, of type date.
-        1.3.- Status, of type selection, with these values: draft, in active
+        1.1.- Historical Date, of type date.
+        1.2.- Status, of type selection, with these values: draft, in active
               and historical. This new field has gotten because it has added a
               workflow to MRP BoM list object.
 
@@ -38,6 +36,7 @@
     historical state.
     when the MRP BoM list is put to active, a record of who has activated,
     and when will include in chatter/log.
+    Also creates a constraint for the sequence field to be unique.
     """,
     "depends": ['mrp',
                 ],
