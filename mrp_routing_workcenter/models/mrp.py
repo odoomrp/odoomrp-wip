cd5cd6d202ab972889b1cbe63b22d4d542d5b0dd
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2014-11-13 09:58:23 +0100

    [IMP mrp_routing_workcenter]

--- mrp_routing_workcenter/models/mrp.py
+++ mrp_routing_workcenter/models/mrp.py
@@ -19,36 +19,8 @@
 from openerp import models, fields, api, _
 
 
-class MrpRouting(models.Model):
-    _inherit = 'mrp.routing'
-
-    workcenter = fields.Many2one('mrp.workcenter', string='Work Center')
-
-
-class MrpWorkcenter(models.Model):
-    _inherit = 'mrp.workcenter'
-
-    capacity_per_cycle = fields.Float(
-        string='Capacity per Cycle Max.', help='Capacity per cycle maximum.')
-    capacity_per_cycle_min = fields.Float(
-        string='Capacity per Cycle Min.', help='Capacity per cycle minimum.')
-
-
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
-
-    @api.one
-    @api.depends('bom_id', 'bom_id.routing_ids')
-    def _compute_permited_routings(self):
-        lines = self.env['mrp.routing']
-        self.permited_routings = lines
-        for routing in self.bom_id.routing_ids:
-            lines |= routing
-        self.permited_routings = lines
-
-    permited_routings = fields.Many2many(
-        'mrp.routing', string='Permited Routings',
-        compute='_compute_permited_routings')
 
     @api.multi
     def product_qty_change_mrp_production(self, product_qty=0,
@@ -73,3 +45,18 @@ class MrpProduction(models.Model):
     def onchange_routing(self):
         if self.routing_id:
             self.product_qty = self.routing_id.workcenter.capacity_per_cycle
+
+
+class MrpRouting(models.Model):
+    _inherit = 'mrp.routing'
+
+    workcenter = fields.Many2one('mrp.workcenter', string='Work Center')
+
+
+class MrpWorkcenter(models.Model):
+    _inherit = 'mrp.workcenter'
+
+    capacity_per_cycle = fields.Float(
+        string='Capacity per Cycle Max.', help='Capacity per cycle maximum.')
+    capacity_per_cycle_min = fields.Float(
+        string='Capacity per Cycle Min.', help='Capacity per cycle minimum.')
