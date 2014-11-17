27fa1ecc3db1a17d09b740cce21511c0cf6c8ee2
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2014-11-17 16:54:58 +0100

    [NEW purchase_requisition_line_menu]

---
+++ purchase_requisition_line_menu/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+{
+    "name": "Purchase Requisition Line Menu",
+    "version": "1.0",
+    "author": "OdooMRP team",
+    "contributors": [
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+        ],
+    "category": "Purchase",
+    "website": "http://www.odoomrp.com",
+    "description": """
+    This module creates the new menu option 'Purchase Requisition Lines'.
+
+    The "Purchase Requisition" field is displayed in the product line, and
+    you can search by product, and requistion.
+    """,
+    "depends": ['purchase',
+                'purchase_requisition',
+                ],
+    "data": ['views/purchase_requisition_line_view.xml',
+             ],
+    "installable": True
+}
