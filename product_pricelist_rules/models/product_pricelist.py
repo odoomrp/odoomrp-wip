a0497d592592e587bf4d654d49e714aeec72812c
oihane <oihanecruce@gmail.com>
2014-11-20 09:16:41 +0100

    [MOD] Working with pricelists

--- product_pricelist_rules/models/product_pricelist.py
+++ product_pricelist_rules/models/product_pricelist.py
@@ -35,14 +35,16 @@ class PricelistItem(models.Model):
     pricelist = fields.Many2one(comodel_name='product.pricelist',
                                 related='price_version_id.pricelist_id',
                                 string='Pricelist', store=True)
-    offer = fields.Many2one(comodel_name='product.pricelist.item.offer',
-                            string='Offer')
+    pricelist_type = fields.Selection(
+        string='Pricelist Type', related='pricelist.type', store=True)
+    offer = fields.Many2one(
+        comodel_name='product.pricelist.item.offer', string='Offer')
     discount = fields.Float('Discount %',
                             digits=dp.get_precision('Product Price'))
     discount2 = fields.Float('Discount 2 %',
                              digits=dp.get_precision('Product Price'))
-    product_ul = fields.Many2one(comodel_name='product.ul',
-                                 string='Logistic Unit')
+    product_ul = fields.Many2one(
+        comodel_name='product.ul', string='Logistic Unit')
     item_formula = fields.Char(compute='_item_formula')
 
     _sql_constraints = [
