83ad04c3f2b279fd5e7e51bbf81ca525ce46722e
avanzosc1 <avanzosc1@avanzosc1-HP-Compaq-6910p.(none)>
2014-11-28 11:50:51 +0100

    [add] mrp_sample

---
+++ mrp_sample/README.rst
@@ -0,0 +1,2 @@
+This module provide a method to record the samples taken on a produce of a
+dump product
---
+++ mrp_sample/models/mrp_production.py
@@ -0,0 +1,35 @@
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from openerp import api, fields, models
+
+
+class MrpProduction(models.Model):
+
+    _inherit = "mrp.production"
+    
+    sample = fields.Many2many('mrp.sample')
+    sample_taken = fields.Boolean('Samples Taken')
+    claim = fields.Many2one('crm.claim')
+    packaging_user = fields.Many2one('res.users')
+
+
+class MrpSample(models.Model):
+    _name = 'mrp.sample'
+
+    name = fields.Char(string='Name')
