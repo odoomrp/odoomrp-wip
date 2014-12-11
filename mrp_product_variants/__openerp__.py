43c1924555e2413121ad1c5ca389596ba6a8520e
oihane <oihanecruce@gmail.com>
2014-12-11 09:17:36 +0100

    [MOD] removed mrp_operations dependance

--- mrp_product_variants/__openerp__.py
+++ mrp_product_variants/__openerp__.py
@@ -22,7 +22,6 @@
     "depends": [
         "product",
         "mrp",
-        "mrp_operations",
         "mrp_operations_extension",
         "product_variants_no_automatic_creation",
         "mrp_production_editable_scheduled_products",
