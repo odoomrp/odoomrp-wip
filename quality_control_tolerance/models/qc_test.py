a13ab764950c033178f7343a75869498b3741f53
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-01-05 03:47:58 +0100

    [FIX] quality_control: Some views [FIX] quality_control_tolerance: Error on same tolerance [IMP] quality_control_tolerance: Colors on inspection lines

--- quality_control_tolerance/models/qc_test.py
+++ quality_control_tolerance/models/qc_test.py
@@ -32,29 +32,54 @@ class QcTestQuestion(models.Model):
     @api.onchange('tolerance_below')
     def onchange_tolerance_below(self):
         diff = self.max_value - self.min_value
-        self.tolerance_percent_below = 100 * self.tolerance_below / diff
-        if self.same_tolerance:
-            self.onchange_same_tolerance()
+        if diff:
+            self.tolerance_percent_below = 100 * self.tolerance_below / diff
+            if self.same_tolerance:
+                self.onchange_same_tolerance()
 
     @api.one
     @api.onchange('tolerance_percent_below')
     def onchange_tolerance_percent_below(self):
         diff = self.max_value - self.min_value
-        self.tolerance_below = self.tolerance_percent_below * diff / 100
-        if self.same_tolerance:
-            self.onchange_same_tolerance()
+        if diff:
+            self.tolerance_below = self.tolerance_percent_below * diff / 100
+            if self.same_tolerance:
+                self.onchange_same_tolerance()
 
     @api.one
     @api.onchange('tolerance_above')
     def onchange_tolerance_above(self):
         diff = self.max_value - self.min_value
-        self.tolerance_percent_above = 100 * self.tolerance_above / diff
+        if diff:
+            self.tolerance_percent_above = 100 * self.tolerance_above / diff
 
     @api.one
     @api.onchange('tolerance_percent_above')
     def onchange_tolerance_percent_above(self):
         diff = self.max_value - self.min_value
-        self.tolerance_above = self.tolerance_percent_above * diff / 100
+        if diff:
+            self.tolerance_above = self.tolerance_percent_above * diff / 100
+
+    def check_same_tolerance(self, vals):
+        vals = vals.copy()
+        if ('tolerance_below' in vals or 'tolerance_percent_below' in vals and
+                vals.get('same_tolerance')):
+            vals['tolerance_above'] = vals.get('tolerance_below')
+            vals['tolerance_percent_above'] = (
+                vals.get('tolerance_percent_below'))
+        return vals
+
+    @api.model
+    def create(self, default):
+        # This is due to a bug in readonly treatment on views
+        default = self.check_same_tolerance(default)
+        return super(QcTestQuestion, self).create(default)
+
+    @api.multi
+    def write(self, vals):
+        # This is due to a bug in readonly treatment on views
+        vals = self.check_same_tolerance(vals)
+        return super(QcTestQuestion, self).write(vals)
 
 
 class QcTestQuestionValue(models.Model):
