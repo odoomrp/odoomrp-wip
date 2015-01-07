eae5d4a02f35d63a5f557aa7b2445ac9a87babdb
oihane <oihanecruce@gmail.com>
2015-01-07 09:41:07 +0100

    [MOD] Tolerable values calculated

--- quality_control_tolerance/models/qc_inspection.py
+++ quality_control_tolerance/models/qc_inspection.py
@@ -13,10 +13,6 @@ class QcInspection(models.Model):
     def _prepare_inspection_line(self, test, line, fill=None):
         res = super(QcInspection, self)._prepare_inspection_line(
             test, line, fill=fill)
-        res['tolerance_below'] = line.tolerance_below
-        res['tolerance_above'] = line.tolerance_above
-        res['tolerance_percent_below'] = line.tolerance_percent_below
-        res['tolerance_percent_above'] = line.tolerance_percent_above
         res['min_value_below'] = line.min_value_below
         res['max_value_above'] = line.max_value_above
         return res
@@ -27,9 +23,8 @@ class QcInspectionLine(models.Model):
 
     @api.one
     @api.depends('quantitative_value', 'min_value', 'max_value',
-                 'tolerance_below', 'test_uom_id', 'tolerance_above',
-                 'tolerance_percent_below', 'question_type',
-                 'tolerance_percent_above', 'uom_id')
+                 'test_uom_id', 'min_value_below', 'max_value_above',
+                 'question_type', 'uom_id')
     def _compute_tolerance_status(self):
         self.tolerance_status = 'not_tolerable'
         if self.question_type == 'quantitative':
@@ -39,13 +34,11 @@ class QcInspectionLine(models.Model):
                 amount = self.env['product.uom']._compute_qty(
                     self.uom_id.id, self.quantitative_value,
                     self.test_uom_id.id)
-            min_tolerable = self.min_value - self.tolerance_below
-            max_tolerable = self.max_value + self.tolerance_above
-            if amount >= min_tolerable:
+            if amount >= self.min_value_below:
                 if amount >= self.min_value:
                     if amount <= self.max_value:
                         self.tolerance_status = 'optimal'
-                    elif amount <= max_tolerable:
+                    elif amount <= self.max_value_above:
                         self.tolerance_status = 'tolerable'
                 else:
                     self.tolerance_status = 'tolerable'
@@ -54,9 +47,7 @@ class QcInspectionLine(models.Model):
 
     @api.one
     @api.depends('possible_ql_values', 'min_value', 'max_value', 'test_uom_id',
-                 'question_type', 'tolerance_below', 'tolerance_above',
-                 'tolerance_percent_below', 'tolerance_percent_above',
-                 'min_value_below', 'max_value_above')
+                 'question_type', 'min_value_below', 'max_value_above')
     def get_valid_values(self):
         if self.question_type == 'qualitative':
             super(QcInspectionLine, self).get_valid_values()
@@ -66,32 +57,16 @@ class QcInspectionLine(models.Model):
                 [_("%s (tolerable)") % x.name for x in self.possible_ql_values
                  if not x.ok and x.tolerance_status == 'tolerable'])
         elif self.question_type == 'quantitative':
-            self.valid_values = "(-%s/%s%%) %s-%s (+%s/%s%%)" % (
-                self.tolerance_below, self.tolerance_percent_below,
-                self.min_value, self.max_value,
-                self.tolerance_above, self.tolerance_percent_above)
+            self.valid_values = "(%s) %s-%s (%s)" % (
+                self.min_value_below, self.min_value, self.max_value,
+                self.max_value_above)
             if self.env.ref("product.group_uom") in self.env.user.groups_id:
                 self.valid_values += " %s" % self.test_uom_id.name
-
-    @api.one
-    @api.depends('min_value', 'max_value', 'tolerance_below',
-                 'tolerance_above')
-    def _min_max_values_tolerance(self):
-        self.min_value_below = self.min_value - self.tolerance_below
-        self.max_value_above = self.max_value + self.tolerance_above
 
     tolerance_status = fields.Selection(
         [('optimal', 'Optimal'),
          ('tolerable', 'Tolerable'),
          ('not_tolerable', 'Not tolerable')],
         string='Tolerance status', compute='_compute_tolerance_status')
-    tolerance_below = fields.Float(string='Tolerance (below)')
-    tolerance_above = fields.Float(string='Tolerance (above)')
-    tolerance_percent_below = fields.Float(string='% tolerance (below)',
-                                           digits=(3, 2))
-    tolerance_percent_above = fields.Float(string='% tolerance (above)',
-                                           digits=(3, 2))
-    min_value_below = fields.Float(
-        string='Min (tolerance applied)', compute='_min_max_values_tolerance')
-    max_value_above = fields.Float(
-        string='Max (tolerance applied)', compute='_min_max_values_tolerance')
+    min_value_below = fields.Float(string='Min. tolerable')
+    max_value_above = fields.Float(string='Max. tolerable')
