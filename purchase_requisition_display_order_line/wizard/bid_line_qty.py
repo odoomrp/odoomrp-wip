d8a7c26698dd5e752e6c5fdfc81a4a90665c8912
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2015-01-21 10:17:27 +0100

    [FIX purchase_requisition_display_order_line]

--- purchase_requisition_display_order_line/wizard/bid_line_qty.py
+++ purchase_requisition_display_order_line/wizard/bid_line_qty.py
@@ -9,11 +9,12 @@ from openerp import models, api
 class BidLineQty(models.TransientModel):
     _inherit = "bid.line.qty"
 
-    @api.one
+    @api.multi
     def change_qty(self):
+        self.ensure_one()
         purchase_line_obj = self.env['purchase.order.line']
         if 'active_ids' in self._context:
             active_ids = self._context['active_ids']
             for line in purchase_line_obj.browse(active_ids):
                 line.write({'product_qty': self.qty})
-        return {'type': 'ir.actions.act_window_close'}
+        return True
