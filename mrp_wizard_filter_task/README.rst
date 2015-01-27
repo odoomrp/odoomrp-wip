3d023781971c20f0205c1d6b12c7114d8b8b098b
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2015-01-27 13:21:47 +0100

    [FIX mrp_wizard_filter_task]

---
+++ mrp_wizard_filter_task/README.rst
@@ -0,0 +1,6 @@
+MRP wizard filter task
+======================
+This module creates the new option menu 'Selecting tasks to impute', in
+Manufacturing.
+By selecting this new menu, a wizard will open to filter tasks work by
+production order, work order, task, and user. You will also create task work.
--- mrp_wizard_filter_task/__openerp__.py
+++ mrp_wizard_filter_task/__openerp__.py
@@ -1,10 +1,14 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
+# Copyright (c)
+#    2015 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
+#    2015 AvanzOsc (http://www.avanzosc.es)
+#
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,27 +16,26 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
     "name": "MRP Wizard Filter Task",
     "version": "1.0",
     "author": "OdooMRP team",
-    "category": "MRP",
     "website": "http://www.odoomrp.com",
-    "description": """
-    This module creates the new option menu 'Selecting tasks to impute', in
-    Manufacturing.
-
-    By selecting this new menu, a wizard will open to filter tasks by
-    production order, work order, and user.
-    """,
+    "contributors": [
+        "Ana Juaristi <ajuaristio@gmail.com>",
+        "Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+        ],
+    "category": "Manufacturing",
     "depends": ['mrp',
                 'project',
                 'mrp_project_link',
+                'mrp_operations_project',
                 ],
-    "data": ['views/project_task_view.xml',
+    "data": ['views/project_task_work_view.xml',
              'wizard/wizard_filter_mrp_task_view.xml',
              ],
     "installable": True
