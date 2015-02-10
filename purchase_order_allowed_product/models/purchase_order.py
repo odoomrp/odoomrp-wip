b1f3daaa30eeea23d544a556268fa549b163bc38
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2015-02-10 20:29:55 +0100

    [FIX purchase_order_allowed_produc, purchase_order_allowed_proudct_with_customersupplierinfo] issue #647

--- purchase_order_allowed_product/models/purchase_order.py
+++ purchase_order_allowed_product/models/purchase_order.py
@@ -8,7 +8,7 @@ from openerp import models, fields, api
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    only_products_allowed = fields.Boolean(
+    only_allowed_products = fields.Boolean(
         string="Use only allowed products in purchase")
     allowed_products = fields.Many2many(
         comodel_name='product.product',
@@ -19,14 +19,13 @@ class PurchaseOrder(models.Model):
         partner_obj = self.env['res.partner']
         result = super(PurchaseOrder, self).onchange_partner_id(partner_id)
         partner = partner_obj.browse(partner_id)
-        value = result['value']
-        value['only_products_allowed'] = partner.purchase_only_allowed
-        result['value'] = value
+        result['value']['only_allowed_products'] = (
+            partner.purchase_only_allowed)
         return result
 
     @api.one
-    @api.onchange('only_products_allowed')
-    def onchange_only_products_allowed(self):
+    @api.onchange('only_allowed_products')
+    def onchange_only_allowed_products(self):
         supplierinfo_obj = self.env['product.supplierinfo']
         product_obj = self.env['product.product']
         allowed_products = []
@@ -36,14 +35,13 @@ class PurchaseOrder(models.Model):
                 allowed_products.append(product.id)
         self.allowed_products = [(6, 0, allowed_products)]
         allowed_products = []
-        if self.only_products_allowed and self.partner_id:
+        if self.only_allowed_products and self.partner_id:
             cond = self._prepare_allowed_product_domain()
             supplierinfo_ids = supplierinfo_obj.search(cond)
             for line in supplierinfo_ids:
-                if line.product_tmpl_id.purchase_ok:
-                    cond = [('product_tmpl_id', '=', line.product_tmpl_id.id)]
-                    products = product_obj.search(cond)
-                    allowed_products.extend(products.ids)
+                cond = [('product_tmpl_id', '=', line.product_tmpl_id.id)]
+                products = product_obj.search(cond)
+                allowed_products.extend(products.ids)
             self.allowed_products = [(6, 0, allowed_products)]
 
     def _prepare_allowed_product_domain(self):
--- purchase_order_allowed_product_with_customersupplierinfo/README.rst
+++ purchase_order_allowed_product_with_customersupplierinfo/README.rst
@@ -1,9 +1,5 @@
 Purchase order allowed product with customer supplier info
 ===========================================================
-* This module adds in purchase orders form the new field "Only products allowed
-  in purchase". When a supplier is selected in purchase order form, the value
-  of this new field, will be that defined in the supplier.
-
-* If this new field is activated in purchase order form, only be able to select
-  products supplied by the supplier. If not activated, you can select any
-  product.
+* This is an auto-install module, to work perfectly in the purchase order form,
+  the filter of search products supplied by supplier, when the module
+  “product_supplierinfo_for_customer” is installed.
