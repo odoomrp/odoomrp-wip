5e46f857676b67161cb5b3430352a37667d343fa
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2015-02-10 20:25:06 +0100

    [FIX sale_order_allowed_product]

--- sale_order_allowed_product/models/sale_order.py
+++ sale_order_allowed_product/models/sale_order.py
@@ -8,7 +8,7 @@ from openerp import models, fields, api
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    only_products_allowed = fields.Boolean(
+    only_allowed_products = fields.Boolean(
         string="Use only allowed products in sale")
     allowed_products = fields.Many2many(
         comodel_name='product.product',
@@ -19,14 +19,12 @@ class SaleOrder(models.Model):
         partner_obj = self.env['res.partner']
         result = super(SaleOrder, self).onchange_partner_id(partner_id)
         partner = partner_obj.browse(partner_id)
-        value = result['value']
-        value['only_products_allowed'] = partner.sale_only_allowed
-        result['value'] = value
+        result['value']['only_allowed_products'] = partner.sale_only_allowed
         return result
 
     @api.one
-    @api.onchange('only_products_allowed')
-    def onchange_only_products_allowed(self):
+    @api.onchange('only_allowed_products')
+    def onchange_only_allowed_products(self):
         supplierinfo_obj = self.env['product.supplierinfo']
         product_obj = self.env['product.product']
         allowed_products = []
@@ -36,13 +34,12 @@ class SaleOrder(models.Model):
                 allowed_products.append(product.id)
         self.allowed_products = allowed_products
         allowed_products = []
-        if self.only_products_allowed and self.partner_id:
+        if self.only_allowed_products and self.partner_id:
             cond = [('type', '=', 'customer'),
                     ('name', '=', self.partner_id.id)]
             supplierinfo_ids = supplierinfo_obj.search(cond)
             for line in supplierinfo_ids:
-                if line.product_tmpl_id.sale_ok:
-                    cond = [('product_tmpl_id', '=', line.product_tmpl_id.id)]
-                    products = product_obj.search(cond)
-                    allowed_products.extend(products.ids)
+                cond = [('product_tmpl_id', '=', line.product_tmpl_id.id)]
+                products = product_obj.search(cond)
+                allowed_products.extend(products.ids)
             self.allowed_products = [(6, 0, allowed_products)]
