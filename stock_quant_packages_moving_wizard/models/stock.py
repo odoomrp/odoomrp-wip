cc16dc7d90457b045e8c5806a09b82e25ecc72d8
oihane <oihanecruce@gmail.com>
2015-03-02 11:09:52 +0100

    [MOD] Create move from zero

--- stock_quant_packages_moving_wizard/models/stock.py
+++ stock_quant_packages_moving_wizard/models/stock.py
@@ -11,11 +11,17 @@ class StockQuant(models.Model):
 
     @api.one
     def move_to(self, dest_location):
-        last_move = self._get_latest_move(self)
-        new_move = last_move.copy()
-        new_move.location_id = new_move.location_dest_id
-        new_move.location_dest_id = dest_location
-        new_move.date_expected = fields.Datetime.now()
-        new_move.date = new_move.date_expected
-        new_move.product_uom_qty = self.qty
+        move_obj = self.env['stock.move']
+        new_move = move_obj.create({
+            'name': 'Move %s to %s' % (self.product_id.name,
+                                       dest_location.name),
+            'product_id': self.product_id.id,
+            'location_id': self.location_id.id,
+            'location_dest_id': dest_location.id,
+            'product_uom_qty': self.qty,
+            'product_uom': self.product_id.uom_id.id,
+            'date_expected': fields.Datetime.now(),
+            'date': fields.Datetime.now(),
+            'quant_ids': [(4, self.id)]
+        })
         new_move.action_done()
