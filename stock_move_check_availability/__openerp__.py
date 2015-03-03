5d996f10d8213876d57acb431030b291805741b3
oihane <oihanecruce@gmail.com>
2015-03-03 13:38:58 +0100

    [MOD] Fixed some problems when there are quants with diferente histories

--- stock_move_check_availability/__openerp__.py
+++ stock_move_check_availability/__openerp__.py
@@ -1,15 +1,38 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
 
 {
-    'name': 'Stock Move Check Availability',
-    'version': '1.0',
-    'description': """
-    This module adds a button to check availability in stock moves.
-    """,
-    'author': 'OdooMRP team',
-    'website': 'http://www.odoomrp.com',
-    "depends": ['stock'],
-    "category": "Generic Modules",
-    "data": ['views/stock_move_view.xml'
-             ],
+    "name": "Stock - Check Availability button on stock moves",
+    "version": "1.0",
+    "depends": [
+        "stock",
+        "stock_quant_merge",
+    ],
+    "author": "OdooMRP team",
+    "website": "http://www.odoomrp.com",
+    "contributors": [
+        "Daniel Campos <danielcampos@avanzosc.es>",
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
+        "Ana Juaristi <ajuaristio@gmail.com>",
+    ],
+    "category": "Warehouse Management",
+    "data": [
+        "views/stock_move_view.xml"
+    ],
     "installable": True,
 }
