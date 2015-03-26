f5c8979cc82aaed6aaa565a6c83400e367f7a3f3
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-03-26 10:59:28 +0100

    [ADD] purchase_packaging_info
    Packaging information in purchases
    ==================================
    
    This module shows related info about primary and secondary packages in purchase
    orders.
    
    Secondary package means that they contain the primary package (that it's to
    say,they are a "super-package").

---
+++ purchase_packaging_info/README.rst
@@ -0,0 +1,19 @@
+Packaging information in purchases
+==================================
+
+This module shows related info about primary and secondary packages in purchase
+orders.
+
+Secondary package means that they contain the primary package (that it's to
+say,they are a "super-package").
+
+Known issues / Roadmap
+----------------------
+* Get logistic unit set in purchase order header as default for lines.
+
+Credits
+=======
+
+Contributors
+------------
+* Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
---
+++ purchase_packaging_info/models/purchase_order.py
@@ -0,0 +1,66 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, fields, api
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    product_ul = fields.Many2one(
+        comodel_name='product.ul', string='Logistic Unit', readonly=True,
+        states={'draft': [('readonly', False)]})
+
+    @api.multi
+    def onchange_partner_id(self, partner_id):
+        res = super(PurchaseOrder, self).onchange_partner_id(partner_id)
+        if partner_id:
+            partner = self.env['res.partner'].browse(partner_id)
+            if not res.get('value'):
+                res['value'] = {}
+            res['value']['product_ul'] = (
+                partner.partner_product_ul.id or
+                partner.commercial_partner_id.partner_product_ul.id)
+        return res
+
+    @api.one
+    @api.onchange('product_ul')
+    def onchange_product_ul(self):
+        for line in self.order_line:
+            line.sec_pack = line.sec_pack or self.product_ul
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    @api.one
+    @api.depends('order_id.product_ul', 'product_id', 'product_qty')
+    def _calculate_packages(self):
+        package_attr = False
+        for attr_value in self.product_id.attribute_value_ids:
+            if attr_value.attribute_id.is_package:
+                package_attr = attr_value
+                break
+        if package_attr:
+            self.pri_pack_qty = (
+                self.product_qty / (package_attr.numeric_value or 1.0))
+            if package_attr.package_product:
+                self.pri_pack = package_attr.package_product
+                for packaging in self.sec_pack.packagings:
+                    if packaging.product == package_attr.package_product:
+                        self.sec_pack_qty = (
+                            self.pri_pack_qty / (
+                                (packaging.ul_qty * packaging.rows) or 1.0))
+
+    pri_pack_qty = fields.Float(
+        string='# Pkg 1', compute='_calculate_packages', digits=(12, 2),
+        store=True)
+    pri_pack = fields.Many2one(
+        comodel_name='product.product', string='Pkg 1',
+        compute='_calculate_packages', readonly=True)
+    sec_pack_qty = fields.Float(
+        string='# Pkg 2', compute='_calculate_packages', digits=(12, 2),
+        store=True)
+    sec_pack = fields.Many2one(
+        comodel_name='product.ul', string='Pkg 2')
