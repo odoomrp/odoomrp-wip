odoomrp/odoomrp-wip

commit e707b8cfc576499fbc1cc834dbf6c39ce1cb3602

[MOD] <stock_inventory_import> made code more simple

stock_inventory_import/models/stock_inventory.py
@@ -15,12 +15,10 @@ class StockInventory(models.Model):
     def _file_lines_processed(self):
         processed = True
         if self.import_lines:
-            processed = False
-            for line in self.import_lines:
-                if not line.fail or (line.fail and
-                                     line.fail_reason != _('No processed')):
-                    processed = True
-                    break
+            processed = all((line.fail or
+                             (line.fail and
+                              line.fail_reason != _('No processed')))
+                            for line in self.import_lines)
         self.processed = processed
 
     imported = fields.Boolean('Imported')
@@ -76,6 +74,6 @@ class StockInventory(models.Model):
         for inventory in self:
             if not inventory.processed:
                 raise exceptions.Warning(
-                    _("Loaded lines must been once processed at least for "
+                    _("Loaded lines must be processed at least one time for "
                       "inventory : %s") % (inventory.name))
             super(StockInventory, inventory).action_done()

stock_inventory_import/__openerp__.py
@@ -26,7 +26,7 @@
         "Daniel Campos <danielcampos@avanzosc.es>",
         "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
         "Ana Juaristi <ajuaristio@gmail.com>",
-
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
     ],
     "website": "http://www.odoomrp.com",
     "depends": [

commit 02685b22f02a9e91fc0c792b4ae6a768e6b45995

[FIX] bug introduced in e707b8cf

stock_inventory_import/models/stock_inventory.py
@@ -15,7 +15,7 @@ class StockInventory(models.Model):
     def _file_lines_processed(self):
         processed = True
         if self.import_lines:
-            processed = all((line.fail or
+            processed = any((not line.fail or
                              (line.fail and
                               line.fail_reason != _('No processed')))
                             for line in self.import_lines)