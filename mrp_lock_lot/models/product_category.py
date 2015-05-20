3c43f4d8f8bde1a8850fa36dce77f459308b62bd
Lionel Sausin <ls@numerigraphe.com>
2015-05-20 15:27:00 +0200

    [IMP] make lot locking modular

--- mrp_lock_lot/models/product_category.py
+++ mrp_lock_lot/models/product_category.py
@@ -12,15 +12,3 @@ class ProductCategory(models.Model):
         string='Block new Serial Numbers/lots',
         help='If checked, future Serial Numbers/lots will be created blocked '
              'by default')
-
-    def _get_parent_default_locked(self):
-        """Locked (including categories)
-
-        @return True when the category or one of the parents demand new lots
-                to be locked"""
-        _locked = self.lot_default_locked
-        categ = self.parent_id
-        while categ and not _locked:
-            _locked = categ.lot_default_locked
-            categ = categ.parent_id
-        return _locked
