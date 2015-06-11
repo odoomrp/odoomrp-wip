e57abd2ed1c92c29fbe0592648391eeefddca901
Daniel-CA <danielcampos@avanzosc.es>
2015-06-11 10:59:38 +0200

    [IMP] Modifications in prepare method

--- mrp_repair_stock_picking_out/models/mrp_repair.py
+++ mrp_repair_stock_picking_out/models/mrp_repair.py
@@ -12,12 +12,11 @@ class MrpRepair(models.Model):
     picking_out = fields.Many2one('stock.picking', string='Picking Out',
                                   readonly=True)
 
-    def _prepare_stock_picking(self, origin, company_id, move_type, partner_id,
-                               picking_type_id):
-        values = {'origin': origin,
-                  'company_id': company_id,
-                  'move_type': move_type,
-                  'partner_id': partner_id,
+    def _prepare_stock_picking(self, move, picking_type_id):
+        values = {'origin': move.origin,
+                  'company_id': move.company_id.id,
+                  'move_type': move.group_id.move_type or 'direct',
+                  'partner_id': move.partner_id.id,
                   'picking_type_id': picking_type_id
                   }
         return values
@@ -33,11 +32,9 @@ class MrpRepair(models.Model):
             StockPicking = self.env['stock.picking']
             StockPickingType = self.env['stock.picking.type']
             outgoing_type = StockPickingType.search(
-                [('code', '=', 'outgoing')])
-            values = self._prepare_stock_picking(
-                self.move_id.origin, self.move_id.company_id.id,
-                self.move_id.group_id.move_type or 'direct',
-                self.move_id.partner_id.id, outgoing_type.id)
+                [('code', '=', 'outgoing')])[:1]
+            values = self._prepare_stock_picking(self.move_id,
+                                                 outgoing_type.id)
             pick = StockPicking.create(values)
             self.picking_out = pick
             self.move_id.write({'picking_type_id': outgoing_type.id,
