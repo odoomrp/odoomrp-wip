bd3bac5c75f06366c494226b08c7f126725819f8
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2015-06-24 09:00:53 +0200

    [IMP] stock_quant_manual_assign: Delete picking operations to confirm the wizard of "quants Manual" and check availability.

---
+++ stock_quant_manual_assign/models/stock_picking.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, api
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.multi
+    def action_assign(self):
+        for pick in self:
+            pick.pack_operation_ids.unlink()
+        return super(StockPicking, self).action_assign()
