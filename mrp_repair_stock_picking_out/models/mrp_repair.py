bac0ab9c46c6e049bb406bc19185f5f3012b4c6e
Daniel-CA <danielcampos@avanzosc.es>
2015-07-03 11:08:23 +0200

    [IMP] mrp_repair_stock_picking_out: Warehouse added for stock.picking.type search

--- mrp_repair_stock_picking_out/models/mrp_repair.py
+++ mrp_repair_stock_picking_out/models/mrp_repair.py
@@ -12,12 +12,16 @@ class MrpRepair(models.Model):
     picking_out = fields.Many2one('stock.picking', string='Picking Out',
                                   readonly=True)
 
-    def _prepare_stock_picking(self, move, picking_type_id):
+    def _prepare_stock_picking(self, move):
+        StockPickingType = self.env['stock.picking.type']
+        outgoing_type = StockPickingType.search(
+            [('code', '=', 'outgoing'),
+             ('warehouse_id', '=', move.warehouse_id.id)])[:1]
         values = {'origin': move.origin,
                   'company_id': move.company_id.id,
                   'move_type': move.group_id.move_type or 'direct',
                   'partner_id': move.partner_id.id,
-                  'picking_type_id': picking_type_id
+                  'picking_type_id': outgoing_type.id
                   }
         return values
 
@@ -28,15 +32,17 @@ class MrpRepair(models.Model):
         @return: True
         """
         res = super(MrpRepair, self).action_repair_done()
+        StockLocation = self.pool.get("stock.location")
+        StockPicking = self.env['stock.picking']
         if self.address_id and self.move_id:
-            StockPicking = self.env['stock.picking']
-            StockPickingType = self.env['stock.picking.type']
-            outgoing_type = StockPickingType.search(
-                [('code', '=', 'outgoing')])[:1]
-            values = self._prepare_stock_picking(self.move_id,
-                                                 outgoing_type.id)
+            wh_id = StockLocation.get_warehouse(
+                self.env.cr, self.env.uid, self.location_dest_id,
+                self.env.context)
+            if not self.move_id.warehouse_id:
+                self.move_id.warehouse_id = wh_id
+            values = self._prepare_stock_picking(self.move_id)
             pick = StockPicking.create(values)
             self.picking_out = pick
-            self.move_id.write({'picking_type_id': outgoing_type.id,
+            self.move_id.write({'picking_type_id': pick.picking_type_id.id,
                                 'picking_id': pick.id})
         return res
