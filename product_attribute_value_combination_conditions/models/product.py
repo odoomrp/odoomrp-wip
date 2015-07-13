d2e2532eb20d46cb28e025e0c7ec9070f68d60cf
mikelarre <mikelarregi@avanzosc.es>
2015-07-13 15:24:53 +0200

    [FIX] some improvements on views, pep8, code readability

--- product_attribute_value_combination_conditions/models/product.py
+++ product_attribute_value_combination_conditions/models/product.py
@@ -4,6 +4,7 @@
 ##############################################################################
 from openerp import models, api, fields, exceptions
 
+
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
@@ -22,12 +23,12 @@ class ProductAttributeLine(models.Model):
 
     @api.one
     @api.depends('product_tmpl_id.attribute_line_ids')
-    def _get_possible_attributes(self):
+    def _get_possible_predecessors(self):
         attrs = self.env['product.attribute']
         for attr_line in self.product_tmpl_id.attribute_line_ids:
             if attr_line.attribute_id != self.attribute_id:
                 attrs |= attr_line.attribute_id
-        self.possible_values = attrs.sorted()
+        self.possible_predecessors = attrs.sorted()
 
     @api.constrains('predecessors', 'initial')
     def check_initial_has_predecessors(self):
@@ -38,11 +39,13 @@ class ProductAttributeLine(models.Model):
     @api.constrains('predecessors', 'predecessors_constraint')
     def check_predecessors_values(self):
         if self.predecessors_constraint - self.possible_predecessor_values:
-            raise exceptions.Warning('There are values that not longer exist')
+            raise exceptions.Warning("There are values in constraint values "
+                                     "that don't belong anymore to the "
+                                     "possible ones")
 
     @api.one
     @api.depends('predecessors')
-    def _get_possible_values(self):
+    def _get_possible_predecessors(self):
         if self.predecessors:
             values = self.env['product.attribute.value'].search(
                 [('attribute_id', 'in', self.predecessors.ids)])
@@ -54,13 +57,13 @@ class ProductAttributeLine(models.Model):
         relation='attribute_predecessors_rel',
         column1='attribute_line',
         column2='product_attribute',
-        domain="[('id', 'in', possible_values[0][2])]")
-    possible_values = fields.Many2many(
+        domain="[('id', 'in', possible_predecessors[0][2])]")
+    possible_predecessors = fields.Many2many(
         comodel_name='product.attribute',
-        relation='attribute_possible_values',
+        relation='attribute_possible_predecessors',
         column1='attribute_line',
         column2='product_attribute',
-        compute='_get_possible_attributes', readonly=True)
+        compute='_get_possible_predecessors', readonly=True)
     predecessors_constraint = fields.Many2many(
         comodel_name='product.attribute.value',
         relation='predecessors_constraint_rel',
@@ -69,9 +72,9 @@ class ProductAttributeLine(models.Model):
         domain="[('id', 'in', possible_predecessor_values[0][2])]")
     possible_predecessor_values = fields.Many2many(
         comodel_name='product.attribute.value',
-        relation='predecessors_constraint_possible_values_rel',
+        relation='predecessors_constraint_possible_predecessors_rel',
         column1='line_id',
-        column2='value_id', compute='_get_possible_values', readonly=True)
+        column2='value_id', compute='_get_possible_predecessors', readonly=True)
     value_constraints = fields.One2many(comodel_name='value.constraint',
                                         inverse_name='attribute_line')
 
@@ -83,7 +86,7 @@ class ValueConstraint(models.Model):
     # @api.one
     # @api.depends('attribute_line.predecessors_constraint')
     # def _get_possible_predecessors_attribute_values(self):
-    #     self.possible_values = self.attribute_line.predecessors_constraint
+    #     self.possible_predecessors = self.attribute_line.predecessors_constraint
     #
     # @api.one
     # @api.depends('attribute_line.value_ids')
@@ -96,15 +99,15 @@ class ValueConstraint(models.Model):
         column1='value_constraint_id',
         column2='attribute_value_id',
         )
-    # possible_values = fields.Many2many(
+    # possible_predecessors = fields.Many2many(
     #     comodel_name='product.attribute.value',
     #     compute='_get_possible_attribute_values', readonly=True)
-    # possible_values = fields.Many2many(
+    # possible_predecessors = fields.Many2many(
     #     comodel_name='product.attribute.value',
     #     related='attribute_line.predecessors_constraint', readonly=True)
     allowed_values = fields.Many2many(
         comodel_name='product.attribute.value',
-        relation='possible_values_rel',
+        relation='possible_predecessors_rel',
         column1='value_constraint_id',
         column2='attribute_value_id',
         )
