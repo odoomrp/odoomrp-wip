251d5d1cf7f4913f54405a3b612ef63b1aa2702a
Daniel-CA <danielcampos@avanzosc.es>
2015-08-03 16:33:21 +0200

    [FIX] mrp_operations_extension: When WO starts the MO state should be 'in_production' not 'ready'

--- mrp_operations_extension/models/mrp_production.py
+++ mrp_operations_extension/models/mrp_production.py
@@ -154,6 +154,6 @@ class MrpProductionWorkcenterLine(models.Model):
                                                  'done']):
             raise exceptions.Warning(
                 _("Missing materials to start the production"))
-        if self.production_id.state == 'confirmed':
-            self.production_id.state = 'ready'
+        if self.production_id.state in ('confirmed', 'ready'):
+            self.production_id.state = 'in_production'
         return super(MrpProductionWorkcenterLine, self).action_start_working()
