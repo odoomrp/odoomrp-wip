5f2a1d5b929d7650075a19d998f5a3078d96b4d4
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2015-09-07 11:20:22 +0200

    [FIX] mrp_operations_extension: Fixed "_operators_number_avg_cost" function, of the model "mrp.production.workcenter.line", for calculating fields: '# Operators' and 'Operator average hour cost'.

--- mrp_operations_extension/models/mrp_workcenter.py
+++ mrp_operations_extension/models/mrp_workcenter.py
@@ -1,21 +1,7 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
+# For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-
 from openerp import models, fields, api
 from openerp.addons import decimal_precision as dp
 
@@ -26,10 +12,12 @@ class MrpWorkcenter(models.Model):
     @api.one
     @api.depends('operators')
     def _operators_number_avg_cost(self):
-        self.op_number = len(self.operators)
+        self.op_number = 0
         op_avg_cost = 0.0
-        for operator in self.operators:
-            op_avg_cost += operator.employee_ids[:1].product_id.standard_price
+        for ope in self.operators:
+            if (ope.employee_ids[0] and ope.employee_ids[0].product_id):
+                self.op_number += 1
+                op_avg_cost += ope.employee_ids[0].product_id.standard_price
         self.op_avg_cost = op_avg_cost / (self.op_number or 1)
 
     pre_op_product = fields.Many2one('product.product',
@@ -42,7 +30,8 @@ class MrpWorkcenter(models.Model):
     operators = fields.Many2many('res.users', 'mrp_wc_operator_rel',
                                  'workcenter_id', 'operator_id', 'Operators')
     op_number = fields.Integer(
-        string='# Operators', compute=_operators_number_avg_cost)
+        string='# Operators', compute='_operators_number_avg_cost')
     op_avg_cost = fields.Float(
         string='Operator average hour cost',
-        digits=dp.get_precision('Product Price'))
+        digits=dp.get_precision('Product Price'),
+        compute='_operators_number_avg_cost')
