877bb419130a6d6ba4d66b4a19ee5d23b71f0c4c
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2015-09-08 09:55:03 +0200

    [IMP] mrp_operations_extension: Put a "onchange" to calculate fields # Operators, and Operator average hour cost, of model mrp.workcenter.

--- mrp_operations_extension/models/mrp_workcenter.py
+++ mrp_operations_extension/models/mrp_workcenter.py
@@ -10,15 +10,16 @@ class MrpWorkcenter(models.Model):
     _inherit = 'mrp.workcenter'
 
     @api.one
-    @api.depends('operators')
+    @api.onchange('operators')
     def _operators_number_avg_cost(self):
-        self.op_number = 0
+        self.op_number = len(self.operators)
+        num_oper = 0
         op_avg_cost = 0.0
         for ope in self.operators:
             if (ope.employee_ids[0] and ope.employee_ids[0].product_id):
-                self.op_number += 1
+                num_oper += 1
                 op_avg_cost += ope.employee_ids[0].product_id.standard_price
-        self.op_avg_cost = op_avg_cost / (self.op_number or 1)
+        self.op_avg_cost = op_avg_cost / (num_oper or 1)
 
     pre_op_product = fields.Many2one('product.product',
                                      string='Pre-operation costing product')
@@ -29,9 +30,6 @@ class MrpWorkcenter(models.Model):
         'operation', 'Routing Operations')
     operators = fields.Many2many('res.users', 'mrp_wc_operator_rel',
                                  'workcenter_id', 'operator_id', 'Operators')
-    op_number = fields.Integer(
-        string='# Operators', compute='_operators_number_avg_cost')
-    op_avg_cost = fields.Float(
-        string='Operator average hour cost',
-        digits=dp.get_precision('Product Price'),
-        compute='_operators_number_avg_cost')
+    op_number = fields.Integer(string='# Operators')
+    op_avg_cost = fields.Float(string='Operator average hour cost',
+                               digits=dp.get_precision('Product Price'))
