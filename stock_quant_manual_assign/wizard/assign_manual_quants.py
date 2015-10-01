28d1f7d181555da5520efabf1db77b28ec0e4ef7
oihane <oihanecruce@gmail.com>
2015-10-01 11:03:50 +0200

    [IMP] stock_quant_manual_assign: Improved code

--- stock_quant_manual_assign/wizard/assign_manual_quants.py
+++ stock_quant_manual_assign/wizard/assign_manual_quants.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from openerp import fields, models, api, exceptions, _
+# (c) 2015 Mikel Arregi - AvanzOSC
+# (c) 2015 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import api, exceptions, fields, models, _
+import openerp.addons.decimal_precision as dp
 
 
 class AssignManualQuants(models.TransientModel):
     _name = 'assign.manual.quants'
 
     def lines_qty(self):
-        total_qty = 0
-        for line in self.quants_lines:
-            if line.selected:
-                total_qty += line.qty
-        return total_qty
+        return sum(self.quants_lines.mapped('qty'))
 
     @api.one
     @api.constrains('quants_lines')
@@ -22,9 +20,8 @@ class AssignManualQuants(models.TransientModel):
             total_qty = self.lines_qty()
             move = self.env['stock.move'].browse(self.env.context['active_id'])
             if total_qty > move.product_uom_qty:
-                raise exceptions.Warning(_('Error'),
-                                         _('Quantity is higher'
-                                           ' than the needed one'))
+                raise exceptions.Warning(
+                    _('Error'), _('Quantity is higher than the needed one'))
 
     @api.depends('quants_lines')
     def get_move_qty(self):
@@ -52,6 +49,7 @@ class AssignManualQuants(models.TransientModel):
 
     @api.model
     def default_get(self, var_fields):
+        res = super(AssignManualQuants, self).default_get(var_fields)
         move = self.env['stock.move'].browse(self.env.context['active_id'])
         available_quants_ids = self.env['stock.quant'].search(
             ['|', ('location_id', '=', move.location_id.id),
@@ -59,12 +57,16 @@ class AssignManualQuants(models.TransientModel):
              ('product_id', '=', move.product_id.id),
              ('qty', '>', 0),
              ('reservation_id', '=', False)])
-        available_quants = [{'quant': x.id} for x in available_quants_ids]
-        available_quants.extend(
-            {'quant': x.id,
-             'selected': True,
-             'qty': x.qty
-             } for x in move.reserved_quant_ids)
+        available_quants = [{
+            'quant': x.id,
+            'location_id': x.location_id.id,
+        } for x in available_quants_ids]
+        available_quants.extend({
+            'quant': x.id,
+            'selected': True,
+            'qty': x.qty,
+            'location_id': x.location_id.id,
+        } for x in move.reserved_quant_ids)
         return {'quants_lines': available_quants}
 
 
@@ -72,21 +74,27 @@ class AssignManualQuantsLines(models.TransientModel):
     _name = 'assign.manual.quants.lines'
     _rec_name = 'quant'
 
+    @api.multi
     @api.onchange('selected')
     def onchange_selected(self):
-            if not self.selected:
-                self.qty = False
-            if self.selected and self.qty == 0:
-                quant_qty = self.quant.qty
-                remaining_qty = self.assign_wizard.move_qty
-                if quant_qty < remaining_qty:
-                    self.qty = quant_qty
-                else:
-                    self.qty = remaining_qty
+        for record in self:
+            if not record.selected:
+                record.qty = False
+            elif not record.qty:
+                quant_qty = record.quant.qty
+                remaining_qty = record.assign_wizard.move_qty
+                record.qty = (quant_qty if quant_qty < remaining_qty else
+                              remaining_qty)
 
-    assign_wizard = fields.Many2one('assign.manual.quants', string='Move',
-                                    required=True, ondelete="cascade")
-    quant = fields.Many2one('stock.quant', string="Quant", required=True,
-                            ondelete='cascade')
-    qty = fields.Float(string='QTY')
-    selected = fields.Boolean(string="Select")
+    assign_wizard = fields.Many2one(
+        comodel_name='assign.manual.quants', string='Move', required=True,
+        ondelete='cascade')
+    quant = fields.Many2one(
+        comodel_name='stock.quant', string='Quant', required=True,
+        ondelete='cascade')
+    location_id = fields.Many2one(
+        comodel_name='stock.location', string='Location',
+        related='quant.location_id', readonly=True)
+    qty = fields.Float(
+        string='QTY', digits=dp.get_precision('Product Unit of Measure'))
+    selected = fields.Boolean(string='Select')
