f7e52019939c133e13b179cb30597ca4d4266990
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-10-22 14:17:04 +0200

    [REM] product_supplierinfo_for_customer: Moved to https://github.com/OCA/product-attribute

--- product_supplierinfo_for_customer/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from . import test_product_supplierinfo_for_customer
