c24a4575ef70d8049e3fd772c716f6c036215ef9
Fouad ELOUAD <f.elouad@agoradeveloppement.com>
2016-01-19 16:56:41 +0000

    Change the string of the field currency

--- purchase_order_last_prices/models/purchase_order.py
+++ purchase_order_last_prices/models/purchase_order.py
@@ -23,7 +23,7 @@ from openerp import fields, models, api
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
     
-    currency = fields.Char(related ='order_id.currency_id.name',string='Devise')
+    currency = fields.Char(related ='order_id.currency_id.name',string='Currency')
 
     @api.multi
     def action_purchase_product_prices(self):
