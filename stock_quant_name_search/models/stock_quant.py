8b9d276c5f448ebacaca075fc04d983b0ef7dc74
oihane <oihanecruce@gmail.com>
2016-01-25 11:37:53 +0100

    [IMP] stock_quant_name_search: use expression for domain creation

--- stock_quant_name_search/models/stock_quant.py
+++ stock_quant_name_search/models/stock_quant.py
@@ -3,6 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import api, models
+from openerp.models import expression
 
 
 class StockQuant(models.Model):
@@ -10,11 +11,10 @@ class StockQuant(models.Model):
 
     @api.model
     def name_search(self, name='', args=None, operator='ilike', limit=100):
-        if not args:
-            args = []
         if not name:
             return super(StockQuant, self).name_search(
                 name=name, args=args, operator=operator, limit=limit)
+        search_domain = []
         quants = self
         product_obj = self.env['product.product']
         product_search = product_obj.name_search(
@@ -24,10 +24,14 @@ class StockQuant(models.Model):
         product_uoms = map(lambda x: x[0], uom_search)
         product_uom_ids = product_obj.search([('uom_id', 'in', product_uoms)])
         product_ids = map(lambda x: x[0], product_search) + product_uom_ids.ids
-        quants |= self.search([('product_id', 'in', product_ids)] + args)
+        search_domain = expression.OR([search_domain,
+                                       [('product_id', 'in', product_ids)]])
         lot_search = self.env['stock.production.lot'].name_search(
             name=name, args=None, operator=operator, limit=limit)
         lot_ids = map(lambda x: x[0], lot_search)
-        quants |= self.search([('lot_id', 'in', lot_ids)] + args)
+        search_domain = expression.OR([search_domain,
+                                       [('lot_id', 'in', lot_ids)]])
+        search_domain = expression.AND([search_domain, args or []])
+        quants = self.search(search_domain, limit=limit)
         result = quants.name_get()
         return result
