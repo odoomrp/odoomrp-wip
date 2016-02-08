e2172da61dd868559a95b6821f78d82f92c733d3
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-02-08 09:07:04 +0100

    [FIX] purchase_product_variants: Propagate product template on MTO

---
+++ purchase_product_variants/models/procurement_order.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import api, models
+
+
+class ProcurementOrder(models.Model):
+    @api.model
+    def _get_po_line_values_from_proc(self, procurement, partner, company,
+                                      schedule_date):
+        """Write also the product template in the created purchase order."""
+        vals = super(ProcurementOrder, self)._get_po_line_values_from_proc(
+            procurement, partner, company, schedule_date)
+        if vals.get('product_id'):
+            product = self.env['product.product'].browse(vals['product_id'])
+            vals['product_template'] = product.product_tmpl_id.id
+        return vals
