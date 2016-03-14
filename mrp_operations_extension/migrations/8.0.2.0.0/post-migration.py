c37cfeb5111cb31e3a9f5ab7741a0db8ac1981fe
oihane <oihanecruce@gmail.com>
2016-03-14 10:24:59 +0100

    [REM] mrp_operations_extension moved to OCA/manufacture

--- mrp_operations_extension/migrations/8.0.2.0.0/post-migration.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) 2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp.addons.mrp_operations_extension import (
-    create_default_routing_workcenter_line)
-
-
-def migrate(cr, version):
-    create_default_routing_workcenter_line(cr)
