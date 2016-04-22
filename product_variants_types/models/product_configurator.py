87e3bf0c8a8458ebf609da28ca2168fe4ac46acf
oihane <oihanecruce@gmail.com>
2016-04-22 13:33:15 +0200

    product_variants_types: updated with configurator data (#1199)

--- product_variants_types/models/product_configurator.py
+++ product_variants_types/models/product_configurator.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# © 2016 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3
 
-from openerp import api, exceptions, models, _
+from openerp import api, exceptions, fields, models, _
 
 
 class ProductConfigurator(models.AbstractModel):
@@ -25,3 +26,33 @@ class ProductConfigurator(models.AbstractModel):
             raise exceptions.ValidationError(
                 _("You have to fill the following attributes:\n%s") %
                 "\n".join(errors))
+
+
+class ProductConfiguratorAttribute(models.Model):
+    _inherit = 'product.configurator.attribute'
+
+    custom_value = fields.Float(string='Custom value')
+    attr_type = fields.Selection(string='Type', store=False,
+                                 related='attribute_id.attr_type')
+
+    def _is_custom_value_in_range(self):
+        if self.attr_type == 'range':
+            return (self.value.min_range <= self.custom_value <=
+                    self.value.max_range)
+        return True
+
+    @api.multi
+    @api.constrains('custom_value', 'attr_type', 'value_id')
+    def _check_value_in_range(self):
+        for record in self:
+            if not record._is_custom_value_in_range():
+                raise exceptions.Warning(
+                    _("Custom value for attribute '%s' must be between %s and"
+                      " %s.")
+                    % (record.attribute_id.name, record.value_id.min_range,
+                       record.value_id.max_range))
+
+    @api.multi
+    @api.onchange('custom_value', 'value_id')
+    def _onchange_custom_value(self):
+        self._check_value_in_range()
