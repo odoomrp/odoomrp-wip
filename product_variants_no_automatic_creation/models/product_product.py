5139e2d19708d86e60734f31035936eaed5b9476
Hugo Santos <hugo.santos@factorlibre.com>
2016-05-10 17:19:54 +0200

    Fix _check_configuration_validity constraint

--- product_variants_no_automatic_creation/models/product_product.py
+++ product_variants_no_automatic_creation/models/product_product.py
@@ -81,7 +81,8 @@ class ProductProduct(models.Model):
         :raises: exceptions.ValidationError: If the check is not valid.
         """
         for product in self:
-            if bool(product.product_tmpl_id.attribute_line_ids -
+            if bool(product.product_tmpl_id.attribute_line_ids.mapped(
+                    'attribute_id') -
                     product.attribute_line_ids.mapped('attribute_id')):
                 raise exceptions.ValidationError(
                     _("You have to fill all the attributes values."))
