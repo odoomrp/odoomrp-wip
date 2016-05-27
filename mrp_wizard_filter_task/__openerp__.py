56ddb4fce8004dc561ea1e54c659ed480660204d
oihane <oihanecruce@gmail.com>
2016-05-27 11:15:46 +0200

    [REM] mrp_project_link moved to OCA/manufacture mrp_project (#1235)
    * [REM] mrp_project_link moved to OCA/manufacture mrp_project
    
    * [IMP] mrp_wizard_filter_task: dependency changed
    
    * [IMP] procurement_plan_project: changed dependency

--- mrp_wizard_filter_task/__openerp__.py
+++ mrp_wizard_filter_task/__openerp__.py
@@ -1,43 +1,30 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-# Copyright (c)
-#    2015 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
-#    2015 AvanzOsc (http://www.avanzosc.es)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2015 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
+# © 2015 AvanzOsc (http://www.avanzosc.es)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 {
-    "name": "MRP Wizard Filter Task",
-    "version": "1.0",
+    "name": "MRP - Task filtering wizard",
+    "version": "8.0.1.0.0",
+    "category": "Manufacturing",
+    "license": "AGPL-3",
     "author": "OdooMRP team,"
               "AvanzOSC,"
               "Serv. Tecnol. Avanzados - Pedro M. Baeza",
     "website": "http://www.odoomrp.com",
     "contributors": [
-        "Ana Juaristi <ajuaristio@gmail.com>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
         "Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>",
         "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
-        ],
-    "category": "Manufacturing",
-    "depends": ['mrp',
-                'project',
-                'mrp_project_link',
-                ],
-    "data": ['views/project_task_work_view.xml',
-             'wizard/wizard_filter_mrp_task_view.xml',
-             ],
-    "installable": True
+    ],
+    "depends": [
+        "mrp",
+        "project",
+        "mrp_project",
+    ],
+    "data": [
+        "views/project_task_work_view.xml",
+        "wizard/wizard_filter_mrp_task_view.xml",
+    ],
+    "installable": True,
 }
--- procurement_plan_project/__openerp__.py
+++ procurement_plan_project/__openerp__.py
@@ -1,26 +1,14 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Procurement Plan Project",
-    "version": "1.0",
-    "depends": ["mrp_project_link",
-                "procurement_plan"],
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp_project",
+        "procurement_plan",
+    ],
     "author": "OdooMRP team,"
               "AvanzOSC,"
               "Serv. Tecnol. Avanzados - Pedro M. Baeza",
@@ -29,8 +17,8 @@
         When the procurements are loaded into a plan it will assign the main
         project.
     """,
-    'data': [],
-    'demo': [],
-    'installable': True,
-    'auto_install': True,
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "auto_install": True,
 }
