c8e8acdb31bdec6101bc995b4f6feadada66ff9a
agaldona <ainaragaldona@avanzosc.es>
2016-06-27 18:02:27 +0200

    [IMP] stock_quant_valuation: new valuation fields for stock history table

--- stock_quant_valuation/tests/test_stock_quant_valuation.py
+++ stock_quant_valuation/tests/test_stock_quant_valuation.py
@@ -3,6 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 import openerp.tests.common as common
+from openerp import fields
 
 
 class TestStockQuantValuation(common.TransactionCase):
@@ -10,6 +11,7 @@ class TestStockQuantValuation(common.TransactionCase):
     def setUp(self):
         super(TestStockQuantValuation, self).setUp()
         self.quant_model = self.env['stock.quant']
+        self.hist_model = self.env['stock.history']
         self.location_model = self.env['stock.location']
         self.product = self.env.ref('product.product_product_34')
         self.location = self.location_model.search([('usage', '=',
@@ -28,3 +30,31 @@ class TestStockQuantValuation(common.TransactionCase):
                          "Incorrect Manual Value for quant.")
         self.assertEqual(quant.real_value, (20 * 5),
                          "Incorrect Real Value for quant.")
+
+    def test_stock_history(self):
+        hist_line = self.hist_model.search([], limit=1)
+        hist_line.product_id.sudo().manual_standard_cost = 40
+        self.assertEqual(hist_line.manual_value, (hist_line.quantity * 40),
+                         "Incorrect Manual Value for history line.")
+        self.assertEqual(hist_line.real_value,
+                         (hist_line.quantity * hist_line.price_unit_on_quant),
+                         "Incorrect Real Value for history line.")
+
+    def test_stock_history_read_group(self):
+        gfields = ['product_id', 'location_id', 'move_id', 'date', 'source',
+                   'quantity', 'inventory_value', 'manual_value', 'real_value']
+        groupby = ['product_id']
+        domain = [('date', '<=', fields.Date.today())]
+        res = self.hist_model.read_group(
+            domain=domain, fields=gfields, groupby=groupby, offset=0,
+            limit=None, orderby=False, lazy=True)
+        if res:
+            line = res[0]
+            line_domain = line.get('__domain', domain)
+            group_lines = self.hist_model.search(line_domain)
+            sum_real = sum([x.real_value for x in group_lines])
+            sum_manual = sum([x.manual_value for x in group_lines])
+            self.assertEqual(line['real_value'], sum_real,
+                             "Real value not correct sum.")
+            self.assertEqual(line['manual_value'], sum_manual,
+                             "Manual value not correct sum.")
