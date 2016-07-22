8e4ac8873aba6e6cc5e6dc2a4a0fd21d0f077a87
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-07-22 20:35:40 +0200

    product_variants_no_automatic_creation: Link quick-created attribute value to the template

--- product_variants_no_automatic_creation/models/__init__.py
+++ product_variants_no_automatic_creation/models/__init__.py
@@ -4,6 +4,7 @@
 from . import pricelist
 from . import product_attribute_line
 from . import product_attribute_price
+from . import product_attribute_value
 from . import product_category
 from . import product_configurator
 from . import product_product
---
+++ product_variants_no_automatic_creation/models/product_attribute_value.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3
+
+from openerp import api, models
+
+
+class ProductAttributeValue(models.Model):
+    _inherit = 'product.attribute.value'
+
+    @api.model
+    def create(self, vals):
+        """Link created attribute value to the associated template if proceed.
+        This happens when quick-creating values from the product configurator.
+        """
+        attr_value = super(ProductAttributeValue, self).create(vals)
+        if self.env.context.get('template_for_attribute_value'):
+            template = self.env['product.template'].browse(
+                self.env.context['template_for_attribute_value'])
+            line = template.attribute_line_ids.filtered(
+                lambda x: x.attribute_id == attr_value.attribute_id)
+            line.value_ids = [(4, attr_value.id)]
+        return attr_value
