90dc9c285c1e5057aca09df1079fd4a03699cbde
Esther Martín <esthermartin001@gmail.com>
2016-08-09 11:28:54 +0200

    [MOD] stock_inventory_import: remove related field, fix code, update view for work fine with preparation_filter

--- stock_inventory_import/models/stock_inventory.py
+++ stock_inventory_import/models/stock_inventory.py
@@ -3,15 +3,20 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import models, fields, api, exceptions, _
+import openerp.addons.decimal_precision as dp
 
 
 class StockInventoryLine(models.Model):
     _inherit = 'stock.inventory.line'
 
-    standard_price = fields.Float(related='product_id.standard_price')
+    standard_price = fields.Float('Cost Price',
+                                  digits=dp.get_precision('Product Price'))
 
     @api.model
     def _resolve_inventory_line(self, inventory_line):
+        product = self.env['product.product'].browse(
+            inventory_line.product_id.id)
+        product.standard_price = inventory_line.standard_price
         move = super(StockInventoryLine,
                      self)._resolve_inventory_line(inventory_line)
         if move:
@@ -28,6 +33,14 @@ class StockInventory(models.Model):
         res = super(StockInventory, self)._get_available_filters()
         res.append(('file', _('By File')))
         return res
+
+    @api.model
+    def _get_inventory_lines(self, inventory):
+        vals = super(StockInventory, self)._get_inventory_lines(inventory)
+        for line in vals:
+            product = self.env['product.product'].browse(line['product_id'])
+            line.update({'standard_price': product.standard_price})
+        return vals
 
     @api.multi
     @api.depends('import_lines')
