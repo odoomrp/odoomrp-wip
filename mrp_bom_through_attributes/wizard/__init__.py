bc3f03b3f0768a220675723cd2fb6016dc628a88
agaldona <ainaragaldona@avanzosc.es>
2016-09-12 10:57:36 +0200

    [IMP] mrp_bom_through_attributes: change production qty wizard take into account attribute raw materials

---
+++ mrp_bom_through_attributes/wizard/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2016 Ainara Galdona <ainaragaldona@avanzosc.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import change_production_qty
