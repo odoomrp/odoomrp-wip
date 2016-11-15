52e9f942265285faa748a2d031bf2a9cf639c7ab
Daniel-CA <danielcampos@avanzosc.es>
2016-11-15 16:54:44 +0100

    [IMP] machine_manager_preventive: code improvement

--- machine_manager_preventive/wizard/create_preventive.py
+++ machine_manager_preventive/wizard/create_preventive.py
@@ -1,21 +1,6 @@
-
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# (c) 2016 Daniel Campos <danielcampos@avanzosc.es> - Avanzosc S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields, api
 
@@ -140,8 +125,9 @@ class PreventiveList(models.TransientModel):
     def _op_count(self):
         return len(self.env.context.get('machi_prevs', 0))
 
-    machi_prevs = fields.One2many('preventive.machine.operation', 'machine',
-                                  'Machine Preventive Operations',
-                                  default=_prev_list, readonly=True)
-    op_count = fields.Integer('Number of operations', default=_op_count,
+    machi_prevs = fields.One2many(
+        comodel_name='preventive.machine.operation', inverse_name='machine',
+        string='Machine Preventive Operations', default=_prev_list,
+        readonly=True)
+    op_count = fields.Integer(string='Number of operations', default=_op_count,
                               readonly=True)
